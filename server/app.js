c543497a0b4892e7f67d773adceac9027ad08357
Mario Martin Rodriguez <mariomartinrodiguez92@gmail.com>
2024-08-13 10:14:51 +0200

    add cors

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import { PORT } from "./env.js";
 
 // Crear la aplicación express
@@ -6,6 +7,8 @@ const app = express();
 
 // Middleware parseo del body de la peticion
 app.use(express.json());
+app.use(cors());
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
