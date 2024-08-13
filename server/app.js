8c4d9a5369949c9b46be50be646ff20309bc2c2b
David Mirás <david_miras@icloud.com>
2024-08-13 12:09:16 +0200

    Corregido app.js

--- server/app.js
+++ server/app.js
@@ -1,15 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-<<<<<<< HEAD
-import { PORT } from "./env.js";
-// Importamos los controladores de errores
-import {
-  notFoundController,
-  errorController,
-} from './src/controllers/errors/index.js';
-=======
 import { PORT } from './env.js';
->>>>>>> 86a4f4a96bb40f1aaed0e8290ec08c20d2781898
+
+import {
+    notFoundController,
+    errorController,
+} from './src/controllers/errors/index.js';
 
 const app = express();
 
@@ -17,15 +13,10 @@ app.use(express.json());
 
 app.use(cors());
 
-<<<<<<< HEAD
-// Middleware de Ruta No Encontrada que ejecuta su función controladora
 app.use(notFoundController);
 
-// Middleware de Error. No nos lo piden
 app.use(errorController);
 
-=======
->>>>>>> 86a4f4a96bb40f1aaed0e8290ec08c20d2781898
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port http://localhost:${PORT}`);
 });
