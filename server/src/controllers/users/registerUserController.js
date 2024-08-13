ded5fdc6b3b340200ad2bb1b6737a4268309c00a
David Mirás <david_miras@icloud.com>
2024-08-13 19:48:59 +0200

    Correcciones

--- server/src/controllers/users/registerUserController.js
+++ server/src/controllers/users/registerUserController.js
@@ -4,25 +4,28 @@ import generateErrorUtil from '../../utils/generateErrorUtil.js';
 import insertUserService from '../../services/users/insertUserService.js';
 
 const registerUserController = async (req, res, next) => {
-  try {
-    const { email, password, username } = req.body;
+    try {
+        const { email, password, userName } = req.body;
 
-    if (!email || !password || !username) {
-      generateErrorUtil('Ni email, ni password, ni username pueden estar vacíos',400);
+        if (!email || !password || !userName) {
+            generateErrorUtil(
+                'Ni email, ni password, ni username pueden estar vacíos',
+                400
+            );
+        }
+
+        const registrationCode = randomstring.generate(30);
+
+        await insertUserService(email, password, userName, registrationCode);
+
+        res.send({
+            status: 'ok',
+            message:
+                'Usuario registrado correctamente. Revise su email para validar la cuenta',
+        });
+    } catch (error) {
+        next(error);
     }
-
-    const registrationCode = randomstring.generate(30);
-
-    await insertUserService(email, password, username, registrationCode);
-
-    res.send({
-      status: 'ok',
-      message:
-        'Usuario registrado correctamente. Revise su email para validar la cuenta',
-    });
-  } catch (error) {
-    next(error);
-  }
 };
 
 export default registerUserController;
--- server/src/services/users/insertUserService.js
+++ server/src/services/users/insertUserService.js
@@ -1,32 +1,34 @@
 import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+
 import getPool from '../../db/getPool.js';
 import generateErrorUtil from '../../utils/generateErrorUtil.js';
 import sendMailUtils from '../../utils/sendMailUtils.js';
 import { PORT } from '../../../env.js';
 
 const insertUserService = async (
-  email,
-  password,
-  username,
-  registrationCode
+    email,
+    password,
+    userName,
+    registrationCode
 ) => {
-  const pool = await getPool();
+    const pool = await getPool();
 
-  const [user] = await pool.query(
-    `
+    const [user] = await pool.query(
+        `
             SELECT id FROM users WHERE email = ?
         `,
-    [email]
-  );
+        [email]
+    );
 
-  if (user.length) {
-    throw generateErrorUtil('El email ya se encuentra registrado', 409);
-  }
+    if (user.length) {
+        throw generateErrorUtil('El email ya se encuentra registrado', 409);
+    }
 
-  const emailSubject = `Activa tu cuenta de ClockYou`;
+    const emailSubject = `Activa tu cuenta de ClockYou`;
 
-  const emaiBody = `
-                !!Bienvenid@ ${username}¡¡
+    const emaiBody = `
+                !!Bienvenid@ ${userName}¡¡
 
                 Gracias por registrarte en ClockYou. Para activar tu cuenta haga click
                 en el siguiente enlace:
@@ -36,17 +38,17 @@ const insertUserService = async (
                 Hecho con ❤ por el equipo de ClockYou.
     `;
 
-  await sendMailUtils(email, emailSubject, emaiBody);
+    await sendMailUtils(email, emailSubject, emaiBody);
 
-  const passwordHashed = await bcrypt.hash(password, 10);
+    const passwordHashed = await bcrypt.hash(password, 10);
 
-  await pool.query(
-    `
-            INSERT INTO users (email, password, registrationCode, username)
-            VALUES (?,?,?,?)
+    await pool.query(
+        `
+            INSERT INTO users (id, email, password, registrationCode, userName)
+            VALUES (?,?,?,?,?)
         `,
-    [email, passwordHashed, registrationCode, username]
-  );
+        [uuid(), email, passwordHashed, registrationCode, userName]
+    );
 };
 
 export default insertUserService;
