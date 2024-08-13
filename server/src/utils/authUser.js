b9edda413e268cf1aebb65238c09a065992c353b
Mario Martin Rodriguez <mariomartinrodiguez92@gmail.com>
2024-08-13 12:23:08 +0200

    authUser.js

---
+++ server/src/utils/authUser.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import generateErrorsUtils from './generateErrorUtil.js'
+
+const authUser = (req, res, next) => {
+    try {
+        
+        const { authorization } = req.headers; 
+
+        if(!authorization) {
+            throw generateErrorsUtils( 'Se esperaba un token por encabezado', 401);
+        }
+
+        let tokeInfo; 
+
+        try {
+            tokeInfo = jwt.verify( authorization, process.env.SECRET)
+        } catch (error) {
+            throw generateErrorsUtils( 'Credenciales inválidas', 401);
+        };
+
+        req.userLogged = tokenInfo;
+
+        next()
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export default authUser;
