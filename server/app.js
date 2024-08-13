d9f37f7c0f3460d5b79c738f8ab271b4bccd8942
Marc-Olle <marc.olle01@gmail.com>
2024-08-13 05:43:59 +0200

    middleware-parseo-del-body

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 import express from "express";
-
 import { PORT } from "./env.js";
 
+// Crear la aplicación express
 const app = express();
+
+// Middleware parseo del body de la peticion
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
