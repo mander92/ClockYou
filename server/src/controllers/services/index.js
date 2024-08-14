8815b71af5dc62a8852c7dc01a91e24af73eebea
Thanh Hang Nguyen <zizie.nguyen@gmail.com>
2024-08-14 10:36:16 +0200

    routers services

---
+++ server/src/controllers/services/index.js
@@ -0,0 +1,5 @@
+import createServiceController from './listServiceController.js';
+import listServiceController from './listServiceController.js';
+import removeServiceController from "./removeServiceController.js";
+
+export { createServiceController, listServiceController, removeServiceController };
