3162b7cf2639fdbe75797ba7131fda24bbc632e1
Mario Martin Rodriguez <mariomartinrodiguez92@gmail.com>
2024-08-15 22:11:19 +0200

    correcion endpoint

--- server/src/services/typeOfServices/selectServiceByType.js
+++ server/src/services/typeOfServices/selectServiceByType.js
@@ -1,19 +1,41 @@
 import getPool from "../../db/getPool.js";
+import generateErrorUtil from "../../utils/generateErrorUtil.js";
 
 const selectServiceByType = async (type, city) => {
-
+    
     const pool = await getPool();
+
+    if(!type){
+        const [ service ] = await pool.query(
+            `
+            SELECT type, description, city FROM typeOfServices WHERE city = ?
+            `,[city]
+        )
+            
+        return service
+    }
+
+    if(!city){
+        const [ service ] = await pool.query(
+            `
+            SELECT type, description, city FROM typeOfServices WHERE type = ?
+            `,[type]
+        )
+            
+        return service
+    }
 
     const [ service ] = await pool.query(
         `
-        SELECT type, description, city FROM typeOfServices WHERE type = ? OR city = ?
-        `, [type, city]
+        SELECT type, description, city FROM typeOfServices WHERE type = ? AND city = ?
+        `,[ type, city]
     );
 
-    console.log(service)
+    if(!service.length){
+        generateErrorUtil('No existen busquedas con los filtros aplicados', 401)
+    }
 
-    return service
-
+    return service;
 
 }
 
