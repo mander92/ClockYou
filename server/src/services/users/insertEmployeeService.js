aa65bb5dea2d4651e70f04a4318de800df4e4604
David Mirás <david_miras@icloud.com>
2024-08-17 22:00:09 +0200

    Añadida activación del usuario empleado

--- server/src/services/users/insertEmployeeService.js
+++ server/src/services/users/insertEmployeeService.js
@@ -1,43 +1,53 @@
-import bcrypt from "bcrypt";
-import { v4 as uuid } from "uuid";
+import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
 
-import getPool from "../../db/getPool.js";
-import generateErrorUtil from "../../utils/generateErrorUtil.js";
+import getPool from '../../db/getPool.js';
+import generateErrorUtil from '../../utils/generateErrorUtil.js';
 
 const insertEmployeeService = async (email, password, userName) => {
-  const pool = await getPool();
+    const pool = await getPool();
 
-  const [user] = await pool.query(
-    `
+    const [user] = await pool.query(
+        `
             SELECT id FROM users WHERE email = ?
         `,
-    [email]
-  );
+        [email]
+    );
 
-  if (user.length) {
-    generateErrorUtil("El email ya se encuentra registrado", 409);
-  }
+    if (user.length) {
+        generateErrorUtil('El email ya se encuentra registrado', 409);
+    }
 
-  const [name] = await pool.query(
-    `
+    const [name] = await pool.query(
+        `
             SELECT id FROM users WHERE userName = ?
         `,
-    [userName]
-  );
+        [userName]
+    );
 
-  if (name.length) {
-    generateErrorUtil("El nombre de usuario ya se encuentra registrado", 409);
-  }
+    if (name.length) {
+        generateErrorUtil(
+            'El nombre de usuario ya se encuentra registrado',
+            409
+        );
+    }
 
-  const passwordHashed = await bcrypt.hash(password, 10);
+    const passwordHashed = await bcrypt.hash(password, 10);
 
-  await pool.query(
-    `
-            INSERT INTO users (id, email, password,  userName, role)
+    await pool.query(
+        `
+            INSERT INTO users (id, email, password, userName, role )
             VALUES (?,?,?,?,?)
         `,
-    [uuid(), email, passwordHashed, userName, "employee"]
-  );
+        [uuid(), email, passwordHashed, userName, 'employee']
+    );
+
+    await pool.query(
+        `
+            UPDATE users SET active=1
+        `,
+        [uuid(), email, passwordHashed, userName, 'employee']
+    );
 };
 
 export default insertEmployeeService;
