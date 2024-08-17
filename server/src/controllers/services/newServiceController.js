5065de13d0b00f660c302bf23d55d56c01e02bfc
Mario Martin Rodriguez <mariomartinrodiguez92@gmail.com>
2024-08-17 14:00:38 +0200

    Joi_in_servicesRoutes

--- server/src/controllers/services/newServiceController.js
+++ server/src/controllers/services/newServiceController.js
@@ -1,8 +1,39 @@
+import Joi from 'joi';
+
 import generateErrorUtil from '../../utils/generateErrorUtil.js';
 import insertServiceService from '../../services/services/insertServiceService.js';
 
 const newServiceController = async (req, res, next) => {
     try {
+
+        const schemaParams = Joi.object().keys({
+            typeId: Joi.string().min(36)
+        })
+
+        const validationParams = schemaParams.validate(req.params);
+
+        if(validationParams.error){
+            generateErrorUtil(validationParams.error.message, 401);
+        };
+
+        const schemaBody = Joi.object().keys({
+            startTime: Joi.string().max(5),
+            endTime: Joi.string().max(5),
+            startDate: Joi.date(),
+            endDate: Joi.date(),
+            description: Joi.string().max(500),
+            address: Joi.string().max(255),
+            city: Joi.string().max(40),
+            postCode: Joi.string().min(5).max(5),
+        });
+
+        const validationBody = schemaBody.validate(req.params);
+
+        if(validationBody.error){
+            generateErrorUtil(validationBody.error.message, 401);
+        };
+
+
         const { typeOfServiceId } = req.params;
         const {
             startTime,
