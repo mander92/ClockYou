3fbd636b2ee20ac179ef94bb36c322f1b8fd5827
David Mirás <david_miras@icloud.com>
2024-08-18 19:29:51 +0200

    Correcciones

--- server/src/controllers/users/listEmployeeController.js
+++ server/src/controllers/users/listEmployeeController.js
@@ -1,36 +1,36 @@
-import generateErrorUtil from "../../utils/generateErrorUtil.js";
-import selectEmployeeService from "../../services/users/selectEmployeeService.js";
+import generateErrorUtil from '../../utils/generateErrorUtil.js';
+import selectEmployeeService from '../../services/users/selectEmployeeService.js';
 
-
-
-const listEmployeeController = async (req,res,next) =>{
+const listEmployeeController = async (req, res, next) => {
     try {
-        
         const isAdmin = req.userLogged.role;
 
-        if(isAdmin !== 'admin'){
-            generateErrorUtil('Acceso denegado bitch', 401)
-        };
+        if (isAdmin !== 'admin') {
+            generateErrorUtil(
+                'Acceso denegado: Se requiere rol de Administrador',
+                401
+            );
+        }
 
         const { job, active, city } = req.query;
 
-        const users  = await selectEmployeeService(job,active, city);
+        const users = await selectEmployeeService(job, active, city);
 
-        if(!users.length){
-            generateErrorUtil('No se ha encontrado en busqueda', 401)
+        if (!users.length) {
+            generateErrorUtil(
+                'No se ha encontrado resultados en la búsqueda',
+                401
+            );
         }
         res.send({
             status: 'ok',
             data: {
-                users
-            }
-            
-        })
-
-
+                users,
+            },
+        });
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
 export default listEmployeeController;
--- server/src/services/users/selectEmployeeService.js
+++ server/src/services/users/selectEmployeeService.js
@@ -1,131 +1,133 @@
-import getPool from "../../db/getPool.js";
+import getPool from '../../db/getPool.js';
 
 const selectEmployeeService = async (job, active, city) => {
-
     const pool = await getPool();
 
-    if(!job && !active && !city){
-        const [ allUsers ] = await pool.query(
+    if (!job && !active && !city) {
+        const [allUsers] = await pool.query(
             `
             SELECT a.city, u.job, u.id, u.firstName, u.lastName
             FROM addresses a
             INNER JOIN 
             users u
             ON a.id = u.addressId
-            WHERE active = 1
+            WHERE active = 1 AND role = 'employee'
             `
         );
 
-        return allUsers
-    };
+        return allUsers;
+    }
 
-    if(job && active && city){
-        const [ allUsers ] = await pool.query(
-            `
-            SELECT a.city, u.job, u.id, u.firstName, u.lastName
-            FROM addresses a
-            INNER JOIN 
-            users u
-            ON a.id = u.addressId
-            WHERE active = ? AND job = ? AND city = ?
-            `,[active,job,city]
-        );
-
-        return allUsers
-    };
-
-    if(job && city){
-        const [ allUsers ] = await pool.query(
+    if (job && active && city) {
+        const [allUsers] = await pool.query(
             `
             SELECT a.city, u.job, u.id, u.firstName, u.lastName
             FROM addresses a
             INNER JOIN 
             users u
             ON a.id = u.addressId
-            WHERE job = ? AND city = ?
-            `,[job,city]
+            WHERE active = ? AND job = ? AND city = ? AND role = 'employee'
+            `,
+            [active, job, city]
         );
 
-        return allUsers
-    };
+        return allUsers;
+    }
 
-    if(job && active){
-        const [ allUsers ] = await pool.query(
+    if (job && city) {
+        const [allUsers] = await pool.query(
             `
             SELECT a.city, u.job, u.id, u.firstName, u.lastName
             FROM addresses a
             INNER JOIN 
             users u
             ON a.id = u.addressId
-            WHERE job = ? AND active = ?
-            `,[job,active]
+            WHERE job = ? AND city = ? AND role = 'employee'
+            `,
+            [job, city]
         );
 
-        return allUsers
-    };
+        return allUsers;
+    }
 
-    if(city && active){
-        const [ allUsers ] = await pool.query(
+    if (job && active) {
+        const [allUsers] = await pool.query(
             `
             SELECT a.city, u.job, u.id, u.firstName, u.lastName
             FROM addresses a
             INNER JOIN 
             users u
             ON a.id = u.addressId
-            WHERE city = ? AND active = ?
-            `,[city,active]
+            WHERE job = ? AND active = ? AND role = 'employee'
+            `,
+            [job, active]
         );
 
-        return allUsers
-    };
+        return allUsers;
+    }
 
-
-    if(!job && !city){
-        const [ allUsers ] = await pool.query(
+    if (city && active) {
+        const [allUsers] = await pool.query(
             `
             SELECT a.city, u.job, u.id, u.firstName, u.lastName
             FROM addresses a
             INNER JOIN 
             users u
             ON a.id = u.addressId
-            WHERE active = ?
-            `,[active]
+            WHERE city = ? AND active = ? AND role = 'employee'
+            `,
+            [city, active]
         );
 
-        return allUsers
-    };
+        return allUsers;
+    }
 
-    if (!city && !active){
-        const [ allUsers ] = await pool.query(
+    if (!job && !city) {
+        const [allUsers] = await pool.query(
             `
             SELECT a.city, u.job, u.id, u.firstName, u.lastName
             FROM addresses a
             INNER JOIN 
             users u
             ON a.id = u.addressId
-            WHERE active = 1 AND job = ?
-            `,[job]
+            WHERE active = ? AND role = 'employee'
+            `,
+            [active]
         );
 
-        return allUsers
-    };
+        return allUsers;
+    }
 
-    if (!job && !active){
-        const [ allUsers ] = await pool.query(
+    if (!city && !active) {
+        const [allUsers] = await pool.query(
             `
             SELECT a.city, u.job, u.id, u.firstName, u.lastName
             FROM addresses a
             INNER JOIN 
             users u
             ON a.id = u.addressId
-            WHERE active = 1 AND city = ?
-            `,[city]
+            WHERE active = 1 AND job = ? AND role = 'employee'
+            `,
+            [job]
         );
 
-        return allUsers
-    };
+        return allUsers;
+    }
 
+    if (!job && !active) {
+        const [allUsers] = await pool.query(
+            `
+            SELECT a.city, u.job, u.id, u.firstName, u.lastName
+            FROM addresses a
+            INNER JOIN 
+            users u
+            ON a.id = u.addressId
+            WHERE active = 1 AND city = ? AND role = 'employee'
+            `,
+            [city]
+        );
 
-
-} 
+        return allUsers;
+    }
+};
 export default selectEmployeeService;
