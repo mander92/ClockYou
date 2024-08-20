a13b78a04132a788ef48298b6899904bb8515ac9
Mario Martin Rodriguez <mariomartinrodiguez92@gmail.com>
2024-08-20 18:48:38 +0200

    listAllService

--- server/src/controllers/services/index.js
+++ server/src/controllers/services/index.js
@@ -1,9 +1,11 @@
 import listServicesController from './listServicesController.js';
 import newServiceController from './newServiceController.js';
 import detailServiceController from './detailServiceController.js';
+import listClientServiceController from './listClientServiceController.js';
 
 export {
     listServicesController,
     newServiceController,
     detailServiceController,
+    listClientServiceController
 };
