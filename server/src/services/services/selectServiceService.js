7708de2cdec849d267a8035d7e03a29e83821160
David Mirás <david_miras@icloud.com>
2024-08-21 00:52:15 +0200

    Corrección selectServiceService

--- server/src/services/services/selectServiceService.js
+++ server/src/services/services/selectServiceService.js
@@ -1,27 +1,29 @@
 import getPool from '../../db/getPool.js';
 
 const selectServiceService = async () => {
-  const pool = await getPool();
+    const pool = await getPool();
 
-  const [services] = await pool.query(
-    `
-            SELECT s.status AS Estado, s.createdAt AS Creación, 
-            t.id AS typeOfServiceId, t.type AS Tipo_Servicio, t.city AS Provincia, t.price AS Precio, 
-            a.id AS addressId , a.address AS Direccón, a.postCode AS CP, a.city AS Ciudad, 
-            s.id AS serviceId, s.date AS Fecha, s.hours AS Horas, s.totalPrice AS Precio_Total, s.comments AS Descripción, 
-            u.id AS userId, u.email AS Email, u.firstName AS Nombre, u.lastName AS Apellidos, u.dni AS DNI, u.phone AS Teléfono
-            FROM addresses a
-            INNER JOIN services s
-            ON a.id = s.addressId
-            INNER JOIN users u
-            ON u.id = s.clientId
-            INNER JOIN typeOfServices t
-            ON s.typeOfServicesId = t.id
-            WHERE status = 'pending' AND s.deletedAt IS NULL
-            `
-  );
+    const [services] = await pool.query(
+        `
+          SELECT s.status AS Estado, s.id AS serviceId, t.id AS typeOfServideId, 
+          u.id AS userId, a.id AS addressId, u.id, s.createdAt AS Creación, 
+          t.type AS Tipo_Servicio, t.city AS Provincia, t.price AS Precio, 
+          s.hours AS Horas, s.totalPrice AS Precio_Total, s.date AS Fecha, 
+          a.city AS Ciudad, a.address AS Dirección, a.postCode AS CP, 
+          s.totalPrice AS Precio_Total, u.firstName AS Nombre, u.lastName AS Apellidos, 
+          u.phone AS Teléfono, u.dni AS DNI, s.comments AS Comentarios
+          FROM addresses a
+          INNER JOIN services s
+          ON a.id = s.addressId
+          INNER JOIN users u
+          ON u.id = s.clientId
+          INNER JOIN typeOfServices t
+          ON s.typeOfServicesId = t.id
+          WHERE status = 'pending' 
+        `
+    );
 
-  return services;
+    return services;
 };
 
 export default selectServiceService;
