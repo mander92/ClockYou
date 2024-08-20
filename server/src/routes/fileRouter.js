9149199b153b9776c79b9573a6fab251c8945615
Gbonn7 <gbonninlettering@gmail.com>
2024-08-20 17:08:55 +0200

    Modificado fileRouter, añadido uploadFileutil y actualizado postman

--- server/src/routes/fileRouter.js
+++ server/src/routes/fileRouter.js
@@ -1,27 +1,9 @@
 import express from 'express';
-
 import authUser from '../middleware/authUser.js';
-
 import fileController from '../controllers/file/fileController.js';
+import upload from '../utils/uploadFileUtil.js';  
 
-import { UPLOADS_DIR } from '../../env.js';
-
-import multer from "multer";
-
-    const storage = multer.diskStorage({
-      destination: function (req, file, cb) {
-        cb(null, `./${UPLOADS_DIR}/`)
-      },
-      filename: function (req, file, cb) {
-        console.log(file);
-        cb(null, Date.now() + file.originalname)
-      }
-    })
-
-    const upload = multer({ storage: storage });
-
-
-const router = express.Router()
+const router = express.Router();
 
 router.post('/file', authUser, upload.single("sampleFile"), fileController);
 
