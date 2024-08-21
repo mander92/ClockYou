2e1e0c874d84525b5d1ef303be2c14dacce40357
Marc-Olle <marc.olle01@gmail.com>
2024-08-21 16:28:19 +0200

    correciones2

--- server/src/routes/shiftRouter.js
+++ server/src/routes/shiftRouter.js
@@ -2,13 +2,13 @@ import express from "express";
 import authUser from "../middleware/authUser.js";
 
 import {
-  insertShiftController,
+  newShiftController,
   listShiftRecordsController,
 } from "../controllers/shiftRecords/index.js";
 
 const router = express.Router();
 
-router.post("/shiftRecords", authUser, insertShiftController);
+router.post("/shiftRecords", authUser, newShiftController);
 
 router.get("/shiftRecords", authUser, listShiftRecordsController);
 
