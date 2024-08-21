529c2c3816bd018cce687b7b48a2d937d01195ab
David Mirás <david_miras@icloud.com>
2024-08-21 18:13:32 +0200

    Corrección de editServiceController y updateService

--- server/src/controllers/services/editServiceController.js
+++ server/src/controllers/services/editServiceController.js
@@ -1,8 +1,7 @@
 import generateErrorUtil from '../../utils/generateErrorUtil.js';
 import updateServiceByIdService from '../../services/services/updateServiceByIdService.js';
 
-
-const editServiceController = async(req, res, next) => {
+const editServiceController = async (req, res, next) => {
     try {
         // const loggedId = req.userLogged.id;
 
@@ -13,16 +12,25 @@ const editServiceController = async(req, res, next) => {
         // }
 
         const { serviceId } = req.params;
-        const { address, postCode, city, comments, date, hours } = req.body;
-        
-        const data = await updateServiceByIdService(serviceId, address, postCode, city, comments, date, hours)
+        const { address, postCode, city, comments, date, hours, startTime } =
+            req.body;
+
+        const data = await updateServiceByIdService(
+            serviceId,
+            address,
+            postCode,
+            city,
+            comments,
+            date,
+            hours,
+            startTime
+        );
 
         res.send({
             status: 'ok',
             message: 'Servicio borrado correctamente',
-            data
+            data,
         });
-
     } catch (error) {
         next(error);
     }
--- server/src/services/services/updateServiceByIdService.js
+++ server/src/services/services/updateServiceByIdService.js
@@ -1,26 +1,52 @@
 import getPool from '../../db/getPool.js';
 
-const updateServiceByIdService = async (serviceId, address, postCode, city, comments, date, hours) => {
-  const pool = await getPool();
+const updateServiceByIdService = async (
+    serviceId,
+    address,
+    postCode,
+    city,
+    comments,
+    date,
+    hours,
+    startTime
+) => {
+    const pool = await getPool();
+
+    const [addressId] = await pool.query(
+        `
+      SELECT addressId FROM services WHERE id = ?`,
+        [serviceId]
+    );
 
     await pool.query(
         `
-        UPDATE services SET comments = ?, date = ?, hours = ?
-        WHERE id = ?
-        `,
-        [comments, date, hours, serviceId]
+    UPDATE addresses SET address = ?, postCode = ?, city = ?
+    WHERE id = ?
+    `,
+        [address, postCode, city, addressId[0].addressId]
     );
 
+    const [typeId] = await pool.query(
+        `
+        SELECT typeOfServicesId FROM services WHERE id = ?`,
+        [serviceId]
+    );
 
-//   await pool.query(
-//     `
-//     UPDATE addresses SET address = ?, postCode = ?, city = ?
-//     WHERE id = ?
-//     `,
-//     [ address, postCode, city, serviceId ]
-//   );
+    const [price] = await pool.query(
+        `
+      SELECT price FROM typeOfServices WHERE id = ?`,
+        [typeId[0].typeOfServicesId]
+    );
 
+    const resultPrice = price[0].price * hours;
 
+    await pool.query(
+        `
+        UPDATE services SET comments = ?, date = ?, hours = ?, startTime = ?, totalPrice = ?
+        WHERE id = ?
+        `,
+        [comments, date, hours, startTime, resultPrice, serviceId]
+    );
 };
 
 export default updateServiceByIdService;
