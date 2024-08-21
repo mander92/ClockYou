0dce2b7f6f0a8795f86c6096db5660deffca1b0f
David Mirás <david_miras@icloud.com>
2024-08-21 21:24:47 +0200

    Correcciones en selectServiceService

--- server/src/services/services/selectServiceService.js
+++ server/src/services/services/selectServiceService.js
@@ -11,7 +11,7 @@ const selectServiceService = async (status) => {
             u.id AS userId, a.id AS addressId, s.createdAt AS Creación, 
             t.type AS Tipo_Servicio, t.city AS Provincia, t.price AS Precio, 
             s.hours AS Horas, s.totalPrice AS Precio_Total, s.date AS Fecha, 
-            a.city AS Ciudad, a.address AS Dirección, a.postCode AS CP, 
+            s.startTime AS Hora_Inicio, a.city AS Ciudad, a.address AS Dirección, a.postCode AS CP, 
             s.totalPrice AS Precio_Total, u.firstName AS Nombre, u.lastName AS Apellidos, 
             u.phone AS Teléfono, u.dni AS DNI, s.comments AS Comentarios
             FROM addresses a
@@ -21,8 +21,10 @@ const selectServiceService = async (status) => {
             ON u.id = s.clientId
             INNER JOIN typeOfServices t
             ON s.typeOfServicesId = t.id
+            ORDER BY s.date, s.startTime
           `
         );
+
         return services;
     }
 
@@ -32,7 +34,7 @@ const selectServiceService = async (status) => {
         u.id AS userId, a.id AS addressId, s.createdAt AS Creación, 
         t.type AS Tipo_Servicio, t.city AS Provincia, t.price AS Precio, 
         s.hours AS Horas, s.totalPrice AS Precio_Total, s.date AS Fecha, 
-        a.city AS Ciudad, a.address AS Dirección, a.postCode AS CP, 
+        s.startTime AS Hora_Inicio, a.city AS Ciudad, a.address AS Dirección, a.postCode AS CP, 
         s.totalPrice AS Precio_Total, u.firstName AS Nombre, u.lastName AS Apellidos, 
         u.phone AS Teléfono, u.dni AS DNI, s.comments AS Comentarios
         FROM addresses a
@@ -43,6 +45,7 @@ const selectServiceService = async (status) => {
         INNER JOIN typeOfServices t
         ON s.typeOfServicesId = t.id
         WHERE s.status = ?
+        ORDER BY s.date, s.startTime
       `,
         [status]
     );
