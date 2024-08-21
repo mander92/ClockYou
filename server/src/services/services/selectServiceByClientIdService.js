4d64a767a31ded0cc8880ec246386f27d762285b
David Mirás <david_miras@icloud.com>
2024-08-21 14:29:36 +0200

    Correcciones selectServiceByClientIdService

--- server/src/services/services/selectServiceByClientIdService.js
+++ server/src/services/services/selectServiceByClientIdService.js
@@ -6,9 +6,9 @@ const selectServiceByClientIdService = async (clientId) => {
 
     const [data] = await pool.query(
         `
-            SELECT s.status AS Estado,  s.createdAt AS Creación, t.type AS Tipo_Servicio, t.city AS Provincia, t.price AS Precio, 
-            s.hours AS Horas, s.totalPrice AS Precio_Total, s.date AS Fecha, a.city AS Ciudad, a.address AS Dirección, a.postCode AS CP, 
-            s.totalPrice AS Precio_Total, u.firstName AS Nombre, u.lastName AS Apellidos, u.phone AS Teléfono, s.comments AS Comentarios
+            SELECT s.status AS Estado,
+            t.type AS Tipo_Servicio, t.city AS Provincia, t.price AS Precio_Hora, s.hours AS Horas_Contratadas, s.totalPrice AS Precio_Total, s.date AS Fecha, s.startTime AS Hora_Inicio, 
+            a.address AS Dirección, a.postCode AS CP, a.city AS Ciudad, s.comments AS Comenatarios, u.email AS Email, u.firstName AS Nombre, u.lastName AS Apellidos, u.phone AS Teléfono
             FROM addresses a
             INNER JOIN services s
             ON a.id = s.addressId
