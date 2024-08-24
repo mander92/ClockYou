b413dddc719fc62cff72822021544bb84c4f8120
fico.prieto <ficoprieto@ficoprieto.com>
2024-08-24 09:43:36 +0200

    correcciones estructura front. El resto de front ya a partir de la semana que viene =)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,14 @@
 import { useState } from 'react';
 import './App.css';
-
-import Header from './components/Layout/Header/Header';
-import Home from './components/Layout/Home/Home';
-import Footer from './components/Layout/Footer/Footer';
-import products from './components/data/products.json';
-
-// importamos products aquí para practicar con las PROPS pero, si solo lo va usar 1 componente de los que se despliegan en este archivo, App.jsx, lo suyo sería importar products en el componente que vaya a usar products (Home, en este caso)
+import './index.css';
 
 const App = () => {
   const [count, setCount] = useState(0);
-  console.log('XXXXXXXX --- ', products);
 
   return (
     <>
-      <Header />
-      <Home products={products} />
-      <Footer />
+      <h1>Hola desde el Front de ClockYou</h1>
+      <h2>Empezando a darle estructura al Front de ClockYou</h2>
 
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
--- client/src/components/Layout/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer>
-      <p>&copy; ClockYou 2024</p>
-    </footer>
-  );
-};
-
-export default Footer;
