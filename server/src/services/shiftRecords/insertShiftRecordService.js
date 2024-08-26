d647046d872985fba31e9fa088f6978aa9f07915
fico.prieto <ficoprieto@ficoprieto.com>
2024-08-26 09:16:31 +0200

    tablas para mails en insertShiftRecordService.js, updateRecoverPasswordService.js, subidas

--- server/src/services/shiftRecords/insertShiftRecordService.js
+++ server/src/services/shiftRecords/insertShiftRecordService.js
@@ -6,48 +6,48 @@ import generateErrorUtil from '../../utils/generateErrorUtil.js';
 import sendMailUtils from '../../utils/sendMailUtil.js';
 
 const insertShiftRecordService = async (serviceId, employeeId) => {
-    const pool = await getPool();
+  const pool = await getPool();
 
-    const [created] = await pool.query(
-        `
+  const [created] = await pool.query(
+    `
         SELECT id FROM shiftRecords WHERE serviceId = ? AND employeeId = ?
         `,
-        [serviceId, employeeId]
-    );
+    [serviceId, employeeId]
+  );
 
-    if (created.length) {
-        generateErrorUtil('El turno ya está asignado', 401);
-    }
+  if (created.length) {
+    generateErrorUtil('El turno ya está asignado', 401);
+  }
 
-    const [employee] = await pool.query(
-        `
+  const [employee] = await pool.query(
+    `
         SELECT id FROM users WHERE id = ? AND active = 1
         `,
-        [employeeId]
-    );
+    [employeeId]
+  );
 
-    if (!employee.length) {
-        generateErrorUtil('El empleado no existe o inactivo', 401);
-    }
+  if (!employee.length) {
+    generateErrorUtil('El empleado no existe o inactivo', 401);
+  }
 
-    await pool.query(
-        `
+  await pool.query(
+    `
         UPDATE services SET status = 'accepted' WHERE id = ?
         `,
-        [serviceId]
-    );
+    [serviceId]
+  );
 
-    const id = uuid();
+  const id = uuid();
 
-    await pool.query(
-        `
+  await pool.query(
+    `
         INSERT INTO shiftRecords(id, employeeId, serviceId) VALUES(?,?,?)
         `,
-        [id, employeeId, serviceId]
-    );
+    [id, employeeId, serviceId]
+  );
 
-    const [pedido] = await pool.query(
-        `
+  const [pedido] = await pool.query(
+    `
             SELECT s.status AS Estado,
             t.type AS Tipo_Servicio, t.city AS Provincia, s.validationCode, s.totalPrice AS Precio_Total, s.date AS Fecha, s.startTime AS Hora_Inicio, a.address AS Dirección, a.postCode AS CP, a.city AS Ciudad, u.email AS Email
             FROM addresses a
@@ -59,29 +59,20 @@ const insertShiftRecordService = async (serviceId, employeeId) => {
             ON s.typeOfServicesId = t.id
             WHERE s.id = ? AND s.deletedAt IS NULL
         `,
-        [serviceId]
-    );
+    [serviceId]
+  );
 
-    const emailSubject = `Su Servicio ha sido aceptado`;
+  const emailSubject = `Su Servicio ha sido aceptado`;
 
-    const emailBody = `
+  const emailBody = `
     <html>
-        <body style="display: flex; justify-content: center; margin: 0;">
-            <div style="text-align: center;">
-                <h1>ClockYou</h1>
-                <h2>Resumen de su pedido</h2>
-                <h3>Tipo De Servicio: ${pedido[0].Tipo_Servicio} en ${pedido[0].Provincia}</h3>
-                <h3>${pedido[0].Fecha} a las ${pedido[0].Hora_Inicio} en Calle: ${pedido[0].Dirección}, ${pedido[0].CP}, ${pedido[0].Ciudad}</h3>
-                <h3>Total:${pedido[0].Precio_Total}€</h3>
-                <h3>Por favor confirme su petición</h3>
-                <h2><a href="http://localhost:${PORT}/services/validate/${pedido[0].validationCode}">AQUÍ</a></h2>
-                <h4>Gracias por confiar en ClockYou.</h4>
-            </div>
+        <body>
+            <table bgcolor="#3c3c3c" width="670" border="0" cellspacing="0" cellpadding="0" align="center" style="margin: 0 auto" > <tbody> <tr> <td> <table bgcolor="#3c3c3c" width="670" border="0" cellspacing="0" cellpadding="0" align="left" > <tbody> <tr> <td align="left" style=" padding: 20px 40px; color: #fff; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; " > <p style=" margin: 10px 0 20px; font-size: 35px; font-weight: bold; " > <img src="https://raw.githubusercontent.com/mander92/ClockYou/main/logo-provisiomal-para-mailing.png" alt="" style="width: 40px; margin: 0 -3px -10px 0" /> ClockYou </p> <p style="margin: 0 0 15px; font-size: 20px"> Resumen de su pedido </p> <p style="margin: 0 0 10px; font-size: 16px"> Tipo De Servicio: <br /> ${pedido[0].Tipo_Servicio} en ${pedido[0].Provincia} </p> <p style="margin: 0 0 10px; font-size: 16px"> ${pedido[0].Fecha} a las ${pedido[0].Hora_Inicio} en Calle: ${pedido[0].Dirección}, ${pedido[0].CP}, ${pedido[0].Ciudad} </p> <p style="margin: 0 0 10px; font-size: 16px"> Total:${pedido[0].Precio_Total}€ </p> <p style="margin: 25px 0 5px; font-size: 18px"> Por favor, confirme su petición haciendo click en el siguiente enlace: </p> <p> <a style=" display: inline-block; margin: 0 0 5px; padding: 10px 25px 15px; background-color: #008aff; font-size: 20px; color: #fff; width: auto; text-decoration: none; font-weight: bold; " href="http://localhost:${PORT}/services/validate/${pedido[0].validationCode}" >Confirmar petición</a > </p> <p style="margin: 50px 0 2px"> Gracias por confiar en ClockYou. </p> <p style="margin: 0 0 10px">&copy; ClockYou 2024</p> </td> </tr> </tbody> </table> </td> </tr> </tbody> </table>
         </body>
     </html>
 `;
 
-    await sendMailUtils(pedido[0].Email, emailSubject, emailBody);
+  await sendMailUtils(pedido[0].Email, emailSubject, emailBody);
 };
 
 export default insertShiftRecordService;
--- server/src/services/users/updateRecoverPasswordService.js
+++ server/src/services/users/updateRecoverPasswordService.js
@@ -2,32 +2,25 @@ import getPool from '../../db/getPool.js';
 import sendMailUtil from '../../utils/sendMailUtil.js';
 
 const updateRecoverPasswwordService = async (email, recoverPasswordCode) => {
-    const pool = await getPool();
+  const pool = await getPool();
 
-    await pool.query(
-        `UPDATE users
+  await pool.query(
+    `UPDATE users
         SET recoverPasswordCode=?
         WHERE email=?`,
-        [recoverPasswordCode, email]
-    );
+    [recoverPasswordCode, email]
+  );
 
-    const subject = 'Recuperación de contraseña ClockYou';
+  const subject = 'Recuperación de contraseña ClockYou';
 
-    const body = `
+  const body = `
     <html>
-        <body style="display: flex; justify-content: center; margin: 0;">
-            <div style="text-align: center;">
-                <h1>ClockYou</h1>
-                <h3>Se ha solicitado la recuperación de la contraseña para este email</h3>
-                <h3>${email}</h3>
-                <h4>Utiliza el siguiente código de recuperación para crear una nueva contraseña</h4>
-                <h4>${recoverPasswordCode}</h4>
-                <h5>Si no ha sido usted, ignore este mensaje.</h5>
-            </div>
+        <body>
+            <table bgcolor="#3c3c3c" width="670" border="0" cellspacing="0" cellpadding="0" align="left" > <tbody> <tr> <td align="left" style=" padding: 20px 40px; color: #fff; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; " > <p style=" margin: 10px 0 20px; font-size: 35px; font-weight: bold; " > <img src="https://raw.githubusercontent.com/mander92/ClockYou/main/logo-provisiomal-para-mailing.png" alt="" style="width: 40px; margin: 0 -3px -10px 0" /> ClockYou </p> <p style="margin: 0 0 5px; font-size: 16px"> Se ha solicitado la recuperación de la contraseña para el siguiente email </p> <p style="margin: 0 0 15px; font-size: 20px; color: #fff"> ${email} </p> <p style="margin: 35px 0 5px; font-size: 16px"> Utilice el siguiente código de recuperación para crear una nueva contraseña: </p> <p style="margin: 0 0 15px; font-size: 20px; color: #fff"> ${recoverPasswordCode} </p> <p style="margin: 70px 0 2px"> Gracias por confiar en ClockYou. </p> <p style="margin: 0 0 10px">&copy; ClockYou 2024</p> </td> </tr> </tbody> </table>
         </body>
     </html>
 `;
-    await sendMailUtil(email, subject, body);
+  await sendMailUtil(email, subject, body);
 };
 
 export default updateRecoverPasswwordService;
