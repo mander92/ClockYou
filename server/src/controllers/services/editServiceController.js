e835c3f8c1c7c06020ed2058f3e03dd02300b611
David Mirás <david_miras@icloud.com>
2024-08-26 17:08:11 +0200

    Correcciónes

--- server/src/controllers/services/editServiceController.js
+++ server/src/controllers/services/editServiceController.js
@@ -1,16 +1,9 @@
 import generateErrorUtil from '../../utils/generateErrorUtil.js';
 import updateServiceByIdService from '../../services/services/updateServiceByIdService.js';
+import Joi from 'joi';
 
 const editServiceController = async (req, res, next) => {
     try {
-        const loggedId = req.userLogged.id;
-
-        const { userId } = req.params;
-
-        if (loggedId !== userId) {
-            generateErrorUtil('Acceso denegado, el token no coincide', 409);
-        }
-
         const schema = Joi.object().keys({
             date: Joi.date().min('now'),
             startTime: Joi.string().pattern(/^([01]\d|2[0-3]):([0-5]\d)$/),
