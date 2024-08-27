e48a87f0ac4a3632d63f82fb4058faf5138ccdff
David Mirás <david_miras@icloud.com>
2024-08-28 01:03:00 +0200

    Corrección

--- server/src/db/initDb.js
+++ server/src/db/initDb.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
 
 import getPool from './getPool.js';
+
 import { ADMIN_EMAIL, ADMIN_PASSWORD } from '../../env.js';
 
 const initDb = async () => {
@@ -11,97 +12,109 @@ const initDb = async () => {
         console.log('Borrando tablas...');
 
         await pool.query(
-            'DROP TABLE IF EXISTS shiftRecords, services, typeOfServices, users, addresses'
+            `
+            DROP TABLE IF EXISTS shiftRecords, services, typeOfServices, users, addresses
+            `
         );
 
         console.log('Creando tablas...');
 
-        await pool.query(`
+        await pool.query(
+            `
             CREATE TABLE IF NOT EXISTS addresses (
-                id CHAR(36) PRIMARY KEY NOT NULL,
-                address VARCHAR(255),
-                postCode VARCHAR(5),
-                city VARCHAR(40),
-                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
-                deletedAt DATETIME NULL
+            id CHAR(36) PRIMARY KEY NOT NULL,
+            address VARCHAR(255),
+            postCode VARCHAR(5),
+            city VARCHAR(40),
+            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+            modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
+            deletedAt DATETIME NULL
             )
-        `);
+            `
+        );
 
-        await pool.query(`
+        await pool.query(
+            `
             CREATE TABLE IF NOT EXISTS users (
-                id CHAR(36) PRIMARY KEY NOT NULL,
-                email VARCHAR(100) UNIQUE NOT NULL,
-                firstName VARCHAR(25),
-                lastName VARCHAR(40),
-                dni VARCHAR(11) UNIQUE,
-                password VARCHAR(255) NOT NULL,
-                phone VARCHAR(15),
-                city VARCHAR(25),
-                role ENUM('admin', 'employee', 'client') DEFAULT 'client',
-                job VARCHAR(20),
-                avatar VARCHAR(100),
-                active BOOLEAN DEFAULT false,
-                registrationCode CHAR(30),
-                recoverPasswordCode CHAR(10),
-                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, 
-                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
-                deletedAt DATETIME NULL
+            email VARCHAR(100) UNIQUE NOT NULL,
+            firstName VARCHAR(25),
+            id CHAR(36) PRIMARY KEY NOT NULL,
+            lastName VARCHAR(40),
+            dni VARCHAR(11) UNIQUE,
+            password VARCHAR(255) NOT NULL,
+            phone VARCHAR(15),
+            city VARCHAR(25),
+            role ENUM('admin', 'employee', 'client') DEFAULT 'client',
+            job VARCHAR(20),
+            avatar VARCHAR(100),
+            active BOOLEAN DEFAULT false,
+            registrationCode CHAR(30),
+            recoverPasswordCode CHAR(10),
+            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, 
+            modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
+            deletedAt DATETIME NULL
             )
-        `);
+            `
+        );
 
-        await pool.query(`
+        await pool.query(
+            `
             CREATE TABLE IF NOT EXISTS typeOfServices (
-                id CHAR(36) PRIMARY KEY NOT NULL,
-                type VARCHAR(255) NOT NULL,
-                price DECIMAL(10,2) NOT NULL,
-                description VARCHAR(500) NOT NULL,
-                city VARCHAR(30) NOT NULL,
-                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
-                deletedAt DATETIME NULL
+            id CHAR(36) PRIMARY KEY NOT NULL,
+            type VARCHAR(255) NOT NULL,
+            price DECIMAL(10,2) NOT NULL,
+            description VARCHAR(500) NOT NULL,
+            city VARCHAR(30) NOT NULL,
+            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+            modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
+            deletedAt DATETIME NULL
             )
-        `);
+            `
+        );
 
-        await pool.query(`
+        await pool.query(
+            `
             CREATE TABLE IF NOT EXISTS services (
-                id CHAR(36) PRIMARY KEY NOT NULL,
-                date CHAR(10) NOT NULL,
-                startTime CHAR(5) NOT NULL,
-                hours INT UNSIGNED NOT NULL,
-                rating INT UNSIGNED,
-                totalPrice VARCHAR(10),
-                comments VARCHAR(255),
-                status ENUM ('accepted', 'rejected', 'pending', 'completed', 'confirmed', 'canceled') DEFAULT 'pending',
-                validationCode VARCHAR(30),
-                clientId CHAR(36) NOT NULL,
-                addressId CHAR(36) NOT NULL,
-                typeOfServicesId CHAR(36) NOT NULL,
-                FOREIGN KEY (clientId) REFERENCES users(id),
-                FOREIGN KEY (addressId) REFERENCES addresses(id),
-                FOREIGN KEY (typeOfServicesId) REFERENCES typeOfServices(id),
-                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
-                deletedAt DATETIME NULL
+            id CHAR(36) PRIMARY KEY NOT NULL,
+            date CHAR(10) NOT NULL,
+            startTime CHAR(5) NOT NULL,
+            hours INT UNSIGNED NOT NULL,
+            rating INT UNSIGNED,
+            totalPrice VARCHAR(10),
+            comments VARCHAR(255),
+            status ENUM ('accepted', 'rejected', 'pending', 'completed', 'confirmed', 'canceled') DEFAULT 'pending',
+            validationCode VARCHAR(30),
+            clientId CHAR(36) NOT NULL,
+            addressId CHAR(36) NOT NULL,
+            typeOfServicesId CHAR(36) NOT NULL,
+            FOREIGN KEY (clientId) REFERENCES users(id),
+            FOREIGN KEY (addressId) REFERENCES addresses(id),
+            FOREIGN KEY (typeOfServicesId) REFERENCES typeOfServices(id),
+            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+            modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
+            deletedAt DATETIME NULL
             )
-        `);
+            `
+        );
 
-        await pool.query(`
+        await pool.query(
+            `
             CREATE TABLE IF NOT EXISTS shiftRecords(
-                id CHAR(36) PRIMARY KEY NOT NULL,
-                clockIn TIME,
-                clockOut TIME,
-                latitude DECIMAL(10,8),
-                longitude DECIMAL(11,8),
-                serviceId CHAR(36) NOT NULL,
-                employeeId CHAR(36) NOT NULL,
-                FOREIGN KEY (serviceId) REFERENCES services(id),
-                FOREIGN KEY (employeeId) REFERENCES users(id),
-                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
-                deletedAt DATETIME NULL
+            id CHAR(36) PRIMARY KEY NOT NULL,
+            clockIn TIME,
+            clockOut TIME,
+            latitude DECIMAL(10,8),
+            longitude DECIMAL(11,8),
+            serviceId CHAR(36) NOT NULL,
+            employeeId CHAR(36) NOT NULL,
+            FOREIGN KEY (serviceId) REFERENCES services(id),
+            FOREIGN KEY (employeeId) REFERENCES users(id),
+            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+            modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
+            deletedAt DATETIME NULL
             )
-        `);
+            `
+        );
 
         const insertTypeOfService = async (
             id,
@@ -282,12 +295,15 @@ const initDb = async () => {
 
         await pool.query(
             `
-            INSERT INTO users (id, email, password, role, active) VALUES (?, ?, ?, ?, ?)`,
+            INSERT INTO users (id, email, password, role, active) VALUES (?, ?, ?, ?, ?)
+            `,
             [uuid(), ADMIN_EMAIL, hashedPass, 'admin', 1]
         );
 
         console.log('¡Tablas creadas!');
+
         console.log('¡Servicios básicos creados!');
+
         console.log('¡ADMIN creado!');
     } catch (err) {
         console.error('Error creando las tablas', err.message, err);
