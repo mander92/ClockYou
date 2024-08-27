25bd4f171363d27e3ca6b03ff6482eee01078a81
David Mirás <david_miras@icloud.com>
2024-08-28 00:51:48 +0200

    Corrección en if

--- server/src/services/services/selectServiceByIdService.js
+++ server/src/services/services/selectServiceByIdService.js
@@ -6,24 +6,23 @@ const selectServiceByIdService = async (serviceId) => {
 
     const [service] = await pool.query(
         `
-            SELECT s.status AS Estado,
-            t.type AS Tipo_Servicio, t.city AS Provincia, t.price AS Precio_Hora, s.hours AS Horas_Contratadas, s.totalPrice AS Precio_Total, s.date AS Fecha, s.startTime AS Hora_Inicio, 
-            a.address AS Dirección, a.postCode AS CP, a.city AS Ciudad, s.comments AS Comenatarios, u.email AS Email, u.firstName AS Nombre, u.lastName AS Apellidos, u.phone AS Teléfono
-            FROM addresses a
-            INNER JOIN services s
-            ON a.id = s.addressId
-            INNER JOIN users u
-            ON u.id = s.clientId
-            INNER JOIN typeOfServices t
-            ON s.typeOfServicesId = t.id
-            WHERE s.id = ? AND s.deletedAt IS NULL
+        SELECT s.status AS Estado,
+        t.type AS Tipo_Servicio, t.city AS Provincia, t.price AS Precio_Hora, s.hours AS Horas_Contratadas, s.totalPrice AS Precio_Total, sdate AS Fecha, s.startTime AS Hora_Inicio, 
+        a.address AS Dirección, a.postCode AS CP, a.city AS Ciudad, s.comments AS Comenatarios, u.email AS Email, u.firstName AS Nombre, ulastName AS Apellidos, u.phone AS Teléfono
+        FROM addresses a
+        INNER JOIN services s
+        ON a.id = s.addressId
+        INNER JOIN users u
+        ON u.id = s.clientId
+        INNER JOIN typeOfServices t
+        ON s.typeOfServicesId = t.id
+        WHERE s.id = ? AND s.deletedAt IS NULL
         `,
         [serviceId]
     );
 
-    if (!service.length) {
+    if (!service.length)
         generateErrorUtil('No existen servicios asociados a ese ID', 404);
-    }
 
     return service[0];
 };
