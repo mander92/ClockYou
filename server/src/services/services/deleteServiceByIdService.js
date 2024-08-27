1b92b43b7e0003e5fd58a9a9be38de706585d451
David Mirás <david_miras@icloud.com>
2024-08-27 16:52:46 +0200

    Comprobar que su estado es pending para poder eliminarlo

--- server/src/services/services/deleteServiceByIdService.js
+++ server/src/services/services/deleteServiceByIdService.js
@@ -1,14 +1,29 @@
 import getPool from '../../db/getPool.js';
+import generateErrorUtil from '../../utils/generateErrorUtil.js';
 
 const deleteServiceByIdService = async (serviceId) => {
-  const pool = await getPool();
+    const pool = await getPool();
 
-  await pool.query(
-    `
-    UPDATE services SET deletedAt = NOW() WHERE id = ?
-    `,
-    [serviceId]
-  );
+    const [verify] = await pool.query(
+        `
+      SELECT id FROM services WHERE status = 'pending' AND id = ?
+      `,
+        [serviceId]
+    );
+
+    if (!verify.length) {
+        generateErrorUtil(
+            'No puedes eliminar el servicio debido a su estado',
+            401
+        );
+    }
+
+    await pool.query(
+        `
+        UPDATE services SET deletedAt = NOW(), status = 'canceled' WHERE id = ? 
+        `,
+        [serviceId]
+    );
 };
 
 export default deleteServiceByIdService;
