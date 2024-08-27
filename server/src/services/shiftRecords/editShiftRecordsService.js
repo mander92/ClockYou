b0967e55d2dcbbb5a824e47c79ee7bb0a0ebac33
David Mirás <david_miras@icloud.com>
2024-08-28 00:48:56 +0200

    Corrección

--- server/src/services/shiftRecords/editShiftRecordsService.js
+++ server/src/services/shiftRecords/editShiftRecordsService.js
@@ -4,12 +4,16 @@ const editShiftRecordsService = async (clockIn, clockOut, shiftRecordId) => {
     const pool = await getPool();
 
     await pool.query(
-        `UPDATE shiftRecords SET clockIn = ?, clockOut = ? WHERE id = ?`,
+        `
+        UPDATE shiftRecords SET clockIn = ?, clockOut = ? WHERE id = ?
+        `,
         [clockIn, clockOut, shiftRecordId]
     );
 
     const [shift] = await pool.query(
-        `SELECT * FROM shiftRecords WHERE id = ?`,
+        `
+        SELECT * FROM shiftRecords WHERE id = ?
+        `,
         [shiftRecordId]
     );
 
