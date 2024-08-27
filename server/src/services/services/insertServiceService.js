33bc0f33845166c085e03afb75431cd5e6dcc5cc
David Mirás <david_miras@icloud.com>
2024-08-28 00:27:55 +0200

    Corrección en if

--- server/src/services/services/insertServiceService.js
+++ server/src/services/services/insertServiceService.js
@@ -25,12 +25,10 @@ const insertServiceService = async (
 
     const [existService] = await pool.query(
         `
-    SELECT id FROM services WHERE typeOfServicesId = ? AND clientId = ? AND date = ? AND startTime = ? AND hours = ? AND deletedAt IS NULL
-    `,
+        SELECT id FROM services WHERE typeOfServicesId = ? AND clientId = ? AND date = ? AND startTime = ? AND hours = ? AND deletedAt IS NULL
+        `,
         [typeOfServiceId, userId, date, startTime, hours]
     );
-
-    console.log(existService);
 
     if (existAddress.length && existService.length)
         generateErrorUtil(
@@ -80,17 +78,17 @@ const insertServiceService = async (
 
     const [data] = await pool.query(
         `
-            SELECT s.status AS Estado,
-            t.type AS Tipo_Servicio, t.city AS Provincia, t.price AS Precio_Hora, s.hours AS Horas_Contratadas, s.totalPrice AS Precio_Total, s.date AS Fecha, s.startTime AS Hora_Inicio, 
-            a.address AS Dirección, a.postCode AS CP, a.city AS Ciudad, s.comments AS Comenatarios, u.email AS Email, u.firstName AS Nombre, u.lastName AS Apellidos, u.phone AS Teléfono
-            FROM addresses a
-            INNER JOIN services s
-            ON a.id = s.addressId
-            INNER JOIN users u
-            ON u.id = s.clientId
-            INNER JOIN typeOfServices t
-            ON s.typeOfServicesId = t.id
-            WHERE u.id = ? AND s.id = ?
+        SELECT s.status AS Estado,
+        t.type AS Tipo_Servicio, t.city AS Provincia, t.price AS Precio_Hora, s.hours AS Horas_Contratadas, s.totalPrice AS Precio_Total, sdate AS Fecha, s.startTime AS Hora_Inicio, 
+        a.address AS Dirección, a.postCode AS CP, a.city AS Ciudad, s.comments AS Comenatarios, u.email AS Email, u.firstName AS Nombre, ulastName AS Apellidos, u.phone AS Teléfono
+        FROM addresses a
+        INNER JOIN services s
+        ON a.id = s.addressId
+        INNER JOIN users u
+        ON u.id = s.clientId
+        INNER JOIN typeOfServices t
+        ON s.typeOfServicesId = t.id
+        WHERE u.id = ? AND s.id = ?
         `,
         [userId, serviceId]
     );
