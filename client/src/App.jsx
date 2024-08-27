f73212967cc51cc9b815e2b8e8090c7330b15934
fico.prieto <ficoprieto@ficoprieto.com>
2024-08-27 10:13:11 +0200

    ruta -about- implementada: localhost:xxxx/About

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
 import './App.css';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         <main className='flex-grow'>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
           </Routes>
         </main>
       </Layout>
