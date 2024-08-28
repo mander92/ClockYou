1331f22f9cb4cd8d63071fe9cbb9bf0f06590ecb
David Mirás <david_miras@icloud.com>
2024-08-28 17:27:29 +0200

    Corrección de envío de email

--- server/src/services/users/insertUserService.js
+++ server/src/services/users/insertUserService.js
@@ -27,18 +27,6 @@ const insertUserService = async (
     if (user.length)
         generateErrorUtil('El email ya se encuentra registrado', 409);
 
-    const emailSubject = `Activa tu cuenta de ClockYou`;
-
-    const emailBody = `
-    <html>
-        <body>
-            <table bgcolor="#3c3c3c" width="670" border="0" cellspacing="0" cellpadding="0" align="center" style="margin: 0 auto" > <tbody> <tr> <td> <table bgcolor="#3c3c3c" width="670" border="0" cellspacing="0" cellpadding="0" align="left" > <tbody> <tr> <td align="left" style=" padding: 20px 40px; color: #fff; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; " > <p style=" margin: 10px 0 20px; font-size: 35px; font-weight: bold; " > <img src="https://raw.githubusercontent.com/mander92/ClockYou/main/logo-provisiomal-para-mailing.png" alt="" style="width: 40px; margin: 0 -3px -7px 0" /> ClockYou </p> <p style="margin: 0 0 5px; font-size: 25px;"> Bienvenid@, ${firstName} ${lastName}!!! </p> <p style="margin: 15px 0 5px; font-size: 20px"> Muchas gracias por registrarte en ClockYou. <span style=" display: block; font-size: 18px; margin: 25px 0 0; " > Para continuar, activa tu cuenta haciendo click en el <br /> siguiente enlace: </span> </p> <p> <a href="http://localhost:${PORT}/users/validate/${registrationCode}" style=" display: inline-block; margin: 0 0 5px; padding: 10px 25px 15px; background-color: #008aff; font-size: 20px; color: #fff; width: auto; text-decoration: none; font-weight: bold; " >Activa tu cuenta</a > </p> <p style="margin: 50px 0 10px">&copy; ClockYou 2024 ❤</p> </td> </tr> </tbody> </table> </td> </tr> </tbody> </table>
-        </body>
-    </html>
-`;
-
-    await sendMailUtil(email, emailSubject, emailBody);
-
     const passwordHashed = await bcrypt.hash(password, 10);
 
     await pool.query(
@@ -57,6 +45,18 @@ const insertUserService = async (
             registrationCode,
         ]
     );
+
+    const emailSubject = `Activa tu cuenta de ClockYou`;
+
+    const emailBody = `
+    <html>
+        <body>
+            <table bgcolor="#3c3c3c" width="670" border="0" cellspacing="0" cellpadding="0" align="center" style="margin: 0 auto" > <tbody> <tr> <td> <table bgcolor="#3c3c3c" width="670" border="0" cellspacing="0" cellpadding="0" align="left" > <tbody> <tr> <td align="left" style=" padding: 20px 40px; color: #fff; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; " > <p style=" margin: 10px 0 20px; font-size: 35px; font-weight: bold; " > <img src="https://raw.githubusercontent.com/mander92/ClockYou/main/logo-provisiomal-para-mailing.png" alt="" style="width: 40px; margin: 0 -3px -7px 0" /> ClockYou </p> <p style="margin: 0 0 5px; font-size: 25px;"> Bienvenid@, ${firstName} ${lastName}!!! </p> <p style="margin: 15px 0 5px; font-size: 20px"> Muchas gracias por registrarte en ClockYou. <span style=" display: block; font-size: 18px; margin: 25px 0 0; " > Para continuar, activa tu cuenta haciendo click en el <br /> siguiente enlace: </span> </p> <p> <a href="http://localhost:${PORT}/users/validate/${registrationCode}" style=" display: inline-block; margin: 0 0 5px; padding: 10px 25px 15px; background-color: #008aff; font-size: 20px; color: #fff; width: auto; text-decoration: none; font-weight: bold; " >Activa tu cuenta</a > </p> <p style="margin: 50px 0 10px">&copy; ClockYou 2024 ❤</p> </td> </tr> </tbody> </table> </td> </tr> </tbody> </table>
+        </body>
+    </html>
+`;
+
+    await sendMailUtil(email, emailSubject, emailBody);
 };
 
 export default insertUserService;
