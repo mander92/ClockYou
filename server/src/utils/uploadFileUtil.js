860aa3a753c88bd87fa8c382a4bf39568c5f9fa9
David Mirás <david_miras@icloud.com>
2024-08-28 17:50:31 +0200

    Eliminar console.log

--- server/src/utils/uploadFileUtil.js
+++ server/src/utils/uploadFileUtil.js
@@ -13,7 +13,6 @@ const storage = multer.diskStorage({
         cb(null, uploadPath);
     },
     filename: function (req, file, cb) {
-        console.log(file);
         cb(null, Date.now() + file.originalname);
     },
 });
