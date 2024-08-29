e746a4e4aa3d1b0f66a370006e157575066e42fb
Marc-Olle <marc.olle01@gmail.com>
2024-08-29 19:50:35 +0200

    recoverpassword

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -1,50 +1,70 @@
 const { VITE_API_URL } = import.meta.env;
 
 export const fetchRegisterService = async (
-    email,
-    firstName,
-    lastName,
-    dni,
-    phone,
-    password
+  email,
+  firstName,
+  lastName,
+  dni,
+  phone,
+  password
 ) => {
-    const res = await fetch(`${VITE_API_URL}/users/register`, {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            email,
-            firstName,
-            lastName,
-            dni,
-            phone,
-            password,
-        }),
-    });
+  const res = await fetch(`${VITE_API_URL}/users/register`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      firstName,
+      lastName,
+      dni,
+      phone,
+      password,
+    }),
+  });
 
-    const body = await res.json();
+  const body = await res.json();
 
-    if (body.status === 'error') {
-        throw new Error(body.message);
-    }
+  if (body.status === "error") {
+    throw new Error(body.message);
+  }
 
-    return body.message;
+  return body.message;
 };
 
 export const fetchActiveUserService = async (registrationCode) => {
-    const res = await fetch(
-        `${VITE_API_URL}/users/validate/${registrationCode}`,
-        {
-            method: 'get',
-        }
-    );
-
-    const body = await res.json();
-
-    if (body.status === 'error') {
-        throw new Error(body.message);
+  const res = await fetch(
+    `${VITE_API_URL}/users/validate/${registrationCode}`,
+    {
+      method: "get",
     }
+  );
 
-    return body.message;
+  const body = await res.json();
+
+  if (body.status === "error") {
+    throw new Error(body.message);
+  }
+
+  return body.message;
+};
+
+export const fetchSendRecoverService = async (email) => {
+  const res = await fetch(`${VITE_API_URL}/users/password/recover`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+    }),
+  });
+
+  const body = await res.json();
+
+  if (body.status === "error") {
+    throw new Error(body.message);
+  }
+
+  return body.message;
 };
