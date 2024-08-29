a93e7e1ac3d8dc31cd386b7da17e96dbe2802e63
David Mirás <david_miras@icloud.com>
2024-08-29 15:38:47 +0200

    Añadir Toaster

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,21 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
 import App from './App.jsx';
+import { Toaster } from 'react-hot-toast';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>
+    <StrictMode>
+        <BrowserRouter>
+            <AuthProvider>
+                <App />
+                <Toaster
+                    position='top-center'
+                    toastOptions={{
+                        duration: 6000,
+                    }}
+                />
+            </AuthProvider>
+        </BrowserRouter>
+    </StrictMode>
 );
