941614e43f4f8a8e4a09b201cb43203d5c24189d
fico.prieto <ficoprieto@ficoprieto.com>
2024-08-29 16:18:05 +0200

    add class container

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -7,131 +7,131 @@ import { fetchRegisterService } from '../../services/userServices';
 import toast from 'react-hot-toast';
 
 const Register = () => {
-    const [email, setEmail] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [dni, setDni] = useState('');
-    const [phone, setPhone] = useState('');
-    const [password, setPassword] = useState('');
-    const [repeatedPassword, setRepeatedPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [dni, setDni] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatedPassword, setRepeatedPassword] = useState('');
 
-    const resetInputs = () => {
-        setEmail('');
-        setFirstName('');
-        setLastName('');
-        setDni('');
-        setPhone('');
-        setPassword('');
-        setRepeatedPassword('');
-    };
+  const resetInputs = () => {
+    setEmail('');
+    setFirstName('');
+    setLastName('');
+    setDni('');
+    setPhone('');
+    setPassword('');
+    setRepeatedPassword('');
+  };
 
-    const handleRegister = async (e) => {
-        try {
-            e.preventDefault();
+  const handleRegister = async (e) => {
+    try {
+      e.preventDefault();
 
-            if (password !== repeatedPassword) {
-                throw new Error('¡Las contraseñas no coinciden!');
-            } else {
-                const message = await fetchRegisterService(
-                    email,
-                    firstName,
-                    lastName,
-                    dni,
-                    phone,
-                    password
-                );
+      if (password !== repeatedPassword) {
+        throw new Error('¡Las contraseñas no coinciden!');
+      } else {
+        const message = await fetchRegisterService(
+          email,
+          firstName,
+          lastName,
+          dni,
+          phone,
+          password
+        );
 
-                toast.success(message);
+        toast.success(message);
 
-                resetInputs();
-            }
-        } catch (err) {
-            toast.error(err.message, {
-                id: 'registerError',
-            });
-        }
-    };
+        resetInputs();
+      }
+    } catch (err) {
+      toast.error(err.message, {
+        id: 'registerError',
+      });
+    }
+  };
 
-    return (
-        <main>
-            <h2>Registro</h2>
+  return (
+    <section className='container'>
+      <h2>Registro</h2>
 
-            <form id='registerForm' onSubmit={handleRegister}>
-                <label htmlFor='email'>Email</label>
-                <input
-                    type='email'
-                    id='email'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder='user@clockYou.com'
-                    required
-                />
+      <form id='registerForm' onSubmit={handleRegister}>
+        <label htmlFor='email'>Email</label>
+        <input
+          type='email'
+          id='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='user@clockYou.com'
+          required
+        />
 
-                <label htmlFor='lastName'>Nombre</label>
-                <input
-                    type='text'
-                    id='firstName'
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    placeholder='Manuel'
-                    required
-                />
+        <label htmlFor='lastName'>Nombre</label>
+        <input
+          type='text'
+          id='firstName'
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          placeholder='Manuel'
+          required
+        />
 
-                <label htmlFor='lastName'>Apellidos</label>
-                <input
-                    type='text'
-                    id='lastName'
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                    placeholder='Pérez Rodríguez'
-                    required
-                />
+        <label htmlFor='lastName'>Apellidos</label>
+        <input
+          type='text'
+          id='lastName'
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          placeholder='Pérez Rodríguez'
+          required
+        />
 
-                <label htmlFor='dni'>DNI</label>
-                <input
-                    type='text'
-                    id='dni'
-                    value={dni}
-                    onChange={(e) => setDni(e.target.value)}
-                    placeholder='24873456Z'
-                    required
-                />
+        <label htmlFor='dni'>DNI</label>
+        <input
+          type='text'
+          id='dni'
+          value={dni}
+          onChange={(e) => setDni(e.target.value)}
+          placeholder='24873456Z'
+          required
+        />
 
-                <label htmlFor='phone'>Teléfono</label>
-                <input
-                    type='tel'
-                    id='phone'
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    placeholder='680458923'
-                    required
-                />
+        <label htmlFor='phone'>Teléfono</label>
+        <input
+          type='tel'
+          id='phone'
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder='680458923'
+          required
+        />
 
-                <label htmlFor='pass'>Contraseña</label>
-                <input
-                    type='password'
-                    id='password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder='jobryp-kapDew-fetho6'
-                    required
-                />
+        <label htmlFor='pass'>Contraseña</label>
+        <input
+          type='password'
+          id='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder='jobryp-kapDew-fetho6'
+          required
+        />
 
-                <label htmlFor='repeatedPassword'>Repetir Contraseña</label>
-                <input
-                    type='password'
-                    id='repeatedPassword'
-                    value={repeatedPassword}
-                    onChange={(e) => setRepeatedPassword(e.target.value)}
-                    placeholder='jobryp-kapDew-fetho6'
-                    required
-                />
+        <label htmlFor='repeatedPassword'>Repetir Contraseña</label>
+        <input
+          type='password'
+          id='repeatedPassword'
+          value={repeatedPassword}
+          onChange={(e) => setRepeatedPassword(e.target.value)}
+          placeholder='jobryp-kapDew-fetho6'
+          required
+        />
 
-                <button>Registrarse</button>
-                <button onClick={resetInputs}>Limpiar</button>
-            </form>
-        </main>
-    );
+        <button>Registrarse</button>
+        <button onClick={resetInputs}>Limpiar</button>
+      </form>
+    </section>
+  );
 };
 
 export default Register;
