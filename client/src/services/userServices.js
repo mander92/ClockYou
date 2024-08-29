ae1f843d0eb2bcf5f2d685f1de3ac10e4d6274b6
David Mirás <david_miras@icloud.com>
2024-08-29 16:03:44 +0200

    Creado service para todos los fetch

---
+++ client/src/services/userServices.js
@@ -0,0 +1,33 @@
+const { VITE_API_URL } = import.meta.env;
+
+export const fetchRegisterService = async (
+    email,
+    firstName,
+    lastName,
+    dni,
+    phone,
+    password
+) => {
+    const res = await fetch(`${VITE_API_URL}/users/register`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email,
+            firstName,
+            lastName,
+            dni,
+            phone,
+            password,
+        }),
+    });
+
+    const body = await res.json();
+
+    if (body.status === 'error') {
+        throw new Error(body.message);
+    }
+
+    return body.message;
+};
