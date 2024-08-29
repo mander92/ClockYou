8f9df3faedc73c59c8040973bf8c7beb7a3d93d7
Thanh Hang Nguyen <zizie.nguyen@gmail.com>
2024-08-29 20:29:29 +0200

    estilo NotFound

--- client/src/pages/NotFound/NotFound.jsx
+++ client/src/pages/NotFound/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
+    <div className='notFound'>
       <h1>Error 404 - Page not found</h1>
       <Link to={"/"}>Vuelve a la página principal</Link>
     </div>
