ab40f4d320334eb1a24df5c243b153767e8c5419
David Mirás <david_miras@icloud.com>
2024-08-29 14:30:48 +0200

    Correcciones

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Layout from './components/Layout/Layout';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Login from './pages/Login/Login';
-import Registro from './pages/Registro/Registro';
+import Register from './pages/Register/Register';
 import Validate from './pages/Validate/Validate.jsx';
 import EditService from './pages/EditService/EditService';
 import NewService from './pages/NewService/NewService';
@@ -13,29 +13,33 @@ import RecoverPassword from './pages/RecoverPassword/RecoverPassword';
 import './App.css';
 
 const App = () => {
-  return (
-    <>
-      <Layout>
-        <main className='flex-grow'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/registro' element={<Registro />} />
-            <Route
-              path='/validation/:registrationCode'
-              element={<Validate />}
-            />
-            <Route path='/editservice' element={<EditService />} />
-            <Route path='/newservice' element={<NewService />} />
-            <Route path='/newtypeofservice' element={<NewTypeOfService />} />
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/recoverpassword' element={<RecoverPassword />} />
-          </Routes>
-        </main>
-      </Layout>
-    </>
-  );
+    return (
+        <Layout>
+            <main className='flex-grow'>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route
+                        path='/validation/:registrationCode'
+                        element={<Validate />}
+                    />
+                    <Route path='/editservice' element={<EditService />} />
+                    <Route path='/newservice' element={<NewService />} />
+                    <Route
+                        path='/newtypeofservice'
+                        element={<NewTypeOfService />}
+                    />
+                    <Route path='/notfound' element={<NotFound />} />
+                    <Route
+                        path='/recoverpassword'
+                        element={<RecoverPassword />}
+                    />
+                </Routes>
+            </main>
+        </Layout>
+    );
 };
 
 export default App;
