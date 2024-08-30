eaca4217bae540556937c9f6bd0f9cc24179ff60
fico.prieto <ficoprieto@ficoprieto.com>
2024-08-30 10:13:49 +0200

    cmabios css en notfound (que estaba way, solo detalles)

--- client/src/pages/RecoverPassword/RecoverPassword.jsx
+++ client/src/pages/RecoverPassword/RecoverPassword.jsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
-
-import { useNavigate } from "react-router-dom";
-
-import toast from "react-hot-toast";
-
-import { fetchSendRecoverService } from "../../services/userServices.js";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { fetchSendRecoverService } from '../../services/userServices.js';
 
 const RecoverPassword = () => {
-  const [email, setEmail] = useState("");
-
+  const [email, setEmail] = useState('');
   const navigate = useNavigate();
 
   const resetInputs = () => {
-    setEmail("");
+    setEmail('');
   };
 
   const handleRecover = async (e) => {
@@ -20,38 +16,36 @@ const RecoverPassword = () => {
       e.preventDefault();
 
       if (!email) {
-        throw new Error("¡Debes ingresar un correo!");
+        throw new Error('¡Debes ingresar un correo!');
       } else {
         const message = await fetchSendRecoverService(email);
-
         toast.success(message);
-
         resetInputs();
 
-        navigate("/");
+        navigate('/');
       }
     } catch (error) {
       toast.error(error.message, {
-        id: "recoverError",
+        id: 'recoverError',
       });
     }
   };
   return (
-    <section className="container">
-      <form id="recoverForm" className="userForm" onSubmit={handleRecover}>
+    <section className='container'>
+      <form id='recoverForm' className='userForm' onSubmit={handleRecover}>
         <fieldset>
           <legend>Recupera tu contraseña</legend>
-          <label htmlFor="email">Email</label>
+          <label htmlFor='email'>Email</label>
           <input
-            type="email"
-            id="email"
+            type='email'
+            id='email'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="user@clockYou.com"
+            placeholder='user@clockYou.com'
             required
           />
         </fieldset>
-        <button type="submit">Enviar</button>
+        <button type='submit'>Enviar</button>
       </form>
     </section>
   );
