751776aa689583699fb24e4e18fdd82ef507a07b
David Mirás <david_miras@icloud.com>
2024-08-31 21:23:59 +0200

    Creación de fetch de services

---
+++ client/src/services/serviceServices.js
@@ -0,0 +1,40 @@
+const { VITE_API_URL } = import.meta.env;
+
+export const fetchInputNewService = async (
+    authToken,
+    typeOfServiceId,
+    dateTime,
+    hours,
+    address,
+    postCode,
+    city,
+    comments
+) => {
+    const res = await fetch(`${VITE_API_URL}/services/${typeOfServiceId}`, {
+        method: 'POST',
+        headers: authToken
+            ? {
+                  Authorization: authToken,
+                  'Content-Type': 'application/json',
+              }
+            : {},
+        body: JSON.stringify({
+            dateTime,
+            hours,
+            address,
+            postCode,
+            city,
+            comments,
+        }),
+    });
+
+    const body = await res.json();
+
+    if (body.status === 'error') {
+        throw new Error(body.message);
+    }
+
+    return body;
+
+    console.log(body);
+};
