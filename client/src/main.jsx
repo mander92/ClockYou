f82c22e41dc83d5c9de95d8a3092ecd05298a9c9
fico.prieto <ficoprieto@ficoprieto.com>
2024-09-01 09:47:24 +0200

    retoques

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,24 +4,23 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
 import App from './App.jsx';
 import { Toaster } from 'react-hot-toast';
-import './index.css';
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <BrowserRouter>
-            <AuthProvider>
-                <App />
-                <Toaster
-                    position='top-center'
-                    toastOptions={{
-                        duration: 5000,
-                        style: {
-                            backgroundColor: 'var(--cerulean)',
-                            color: 'var(--white-color)',
-                        },
-                    }}
-                />
-            </AuthProvider>
-        </BrowserRouter>
-    </StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+        <Toaster
+          position='top-center'
+          toastOptions={{
+            duration: 7000,
+            style: {
+              backgroundColor: 'var(--cerulean)',
+              color: 'var(--white-color)',
+            },
+          }}
+        />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 );
