61cedb4aa846b5785e79cfb1195ecc0b9fbbce4e
David Mirás <david_miras@icloud.com>
2024-09-02 15:15:32 +0200

    Mejora en filtros

--- client/src/components/TypeOfServices/TypeOfServices.jsx
+++ client/src/components/TypeOfServices/TypeOfServices.jsx
@@ -5,151 +5,129 @@ import { fetchAllTypeOfServices } from '../../services/typeOfServiceServices';
 import toast from 'react-hot-toast';
 
 const TypeOfServices = () => {
-  const [data, setData] = useState([]);
-  const [city, setCity] = useState('');
-  const [typeOfService, setTypeOfService] = useState('');
-  const [price, setPrice] = useState('');
+    const [data, setData] = useState([]);
+    const [city, setCity] = useState('');
+    const [type, setType] = useState('');
+    const [price, setPrice] = useState('');
 
-  useEffect(() => {
-    const getAllTypeOfServices = async () => {
-      try {
-        const data = await fetchAllTypeOfServices();
-        setData(data);
-      } catch (error) {
-        toast.error(error.message, {
-          id: 'error',
-        });
-      }
-    };
+    useEffect(() => {
+        const getTypeOfServices = async () => {
+            const searchParams = new URLSearchParams({
+                city: city,
+                type: type,
+                price: price,
+            });
+            const searchParamsToString = searchParams.toString();
+            try {
+                const data = await fetchAllTypeOfServices(searchParamsToString);
+                setData(data);
+            } catch (error) {
+                toast.error(error.message, {
+                    id: 'error',
+                });
+            }
+        };
 
-    getAllTypeOfServices();
-  }, []);
+        getTypeOfServices();
+    }, [city, type, price]);
 
-  const handleFilters = async (e) => {
-    e.preventDefault();
-    if (
-      city === 'Ciudad:' ||
-      typeOfService === 'Tipo de Servicio:' ||
-      price === 'Precio:'
-    ) {
-      alert('aaaaaaaaaaaaaa');
-    }
+    const citiesNoRepeated = [...new Set(data.map((item) => item.city))];
+    const typeNoRepeated = [...new Set(data.map((item) => item.type))];
 
-    const searchParams = new URLSearchParams({
-      city: city,
-      type: typeOfService,
-      price: price,
-    });
-    const searchParamsToString = searchParams.toString();
-    // console.log(`${VITE_API_URL}/typeOfServices?${searchParamsToString}`);
+    return (
+        <div className='container'>
+            <h2>Todos los Servicios</h2>
+            <div>
+                <form className='form filterServicesForm'>
+                    <select
+                        name='city'
+                        id='city'
+                        value={city}
+                        onChange={(e) => {
+                            setCity(e.target.value);
+                        }}
+                    >
+                        <option value='' disabled>
+                            Ciudad:
+                        </option>
+                        {citiesNoRepeated.map((city) => {
+                            return (
+                                <option key={city} value={city}>
+                                    {city}
+                                </option>
+                            );
+                        })}
+                    </select>
 
-    try {
-      const fetchAFilterTypeOfServices = async () => {
-        console.log(searchParamsToString);
+                    <select
+                        name='typeOfService'
+                        id='typeOfService'
+                        value={type}
+                        onChange={(e) => {
+                            setType(e.target.value);
+                        }}
+                    >
+                        <option value='' disabled>
+                            Tipo de Servicio:
+                        </option>
+                        {typeNoRepeated.map((type) => {
+                            return (
+                                <option key={type} value={type}>
+                                    {type}
+                                </option>
+                            );
+                        })}
+                    </select>
 
-        const res = await fetch(
-          `${VITE_API_URL}/typeOfServices?${searchParamsToString}`
-        );
-        const body = await res.json();
+                    <select
+                        name='precio'
+                        id='precio'
+                        value={price}
+                        onChange={(e) => {
+                            setPrice(e.target.value);
+                        }}
+                    >
+                        <option value='' disabled>
+                            Precio:
+                        </option>
+                        <option value='ASC'>Ascendente</option>
+                        <option value='DESC'>Descendente</option>
+                    </select>
+                </form>
+            </div>
 
-        if (body.status === 'error') {
-          throw new Error(body.message);
-        }
+            <div>
+                <ul className='gridClockYou'>
+                    {data.map((item) => {
+                        return (
+                            <li
+                                key={item.id}
+                                className='flex flex-col place-content-between'
+                            >
+                                <img
+                                    src={`${VITE_API_URL}/${item.image}`}
+                                    alt={item.description}
+                                />
+                                <h3 className='text-2xl'>{item.type}</h3>
 
-        setData(body.data);
-      };
+                                <p className='grow'>{item.description}</p>
+                                <p className='text-1xl font-black pt-3 pb-1'>
+                                    {item.city}
+                                </p>
 
-      fetchAFilterTypeOfServices();
-    } catch (error) {}
-  };
-
-  // console.log(city);
-  // if !city no hay query
-
-  const citiesNoRepeated = [...new Set(data.map((item) => item.city))];
-  const typeNoRepeated = [...new Set(data.map((item) => item.type))];
-
-  return (
-    <div className='container'>
-      <h2>Todos los Servicios</h2>
-      <div>
-        <form className='form filterServicesForm' onSubmit={handleFilters}>
-          <select
-            name='city'
-            id='city'
-            value={city}
-            onChange={(e) => {
-              setCity(e.target.value);
-            }}
-          >
-            <option>Ciudad:</option>
-            {citiesNoRepeated.map((city) => {
-              return (
-                <option key={city} value={city}>
-                  {city}
-                </option>
-              );
-            })}
-          </select>
-
-          <select
-            name='typeOfService'
-            id='typeOfService'
-            value={typeOfService}
-            onChange={(e) => {
-              setTypeOfService(e.target.value);
-            }}
-          >
-            <option>Tipo de Servicio:</option>
-            {typeNoRepeated.map((type) => {
-              return (
-                <option key={type} value={type}>
-                  {type}
-                </option>
-              );
-            })}
-          </select>
-
-          <select
-            name='precio'
-            id='precio'
-            value={price}
-            onChange={(e) => {
-              setPrice(e.target.value);
-            }}
-          >
-            <option>Precio:</option>
-            <option value='ASC'>Ascendente</option>
-            <option value='DES'>Descendente</option>
-          </select>
-
-          <button>Buscar</button>
-        </form>
-      </div>
-
-      <div>
-        <ul className='gridClockYou'>
-          {data.map((item) => {
-            return (
-              <li key={item.id} className='flex flex-col place-content-between'>
-                <img
-                  src={`${VITE_API_URL}/${item.image}`}
-                  alt={item.description}
-                />
-                <h3 className='text-2xl'>{item.type}</h3>
-
-                <p className='grow'>{item.description}</p>
-                <p className='text-1xl font-black pt-3 pb-1'>{item.city}</p>
-
-                <p>{item.price}€</p>
-                <a href={`${VITE_CLIENT_URL}/typeOfServices/${item.id}`}>Ver</a>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
-  );
+                                <p>{item.price}€</p>
+                                <a
+                                    href={`${VITE_CLIENT_URL}/typeOfServices/${item.id}`}
+                                >
+                                    Ver
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default TypeOfServices;
--- client/src/services/typeOfServiceServices.js
+++ client/src/services/typeOfServiceServices.js
@@ -1,23 +1,27 @@
 const { VITE_API_URL } = import.meta.env;
 
 export const fetchTypeOfService = async (typeOfServiceId) => {
-  const res = await fetch(`${VITE_API_URL}/typeOfServices/${typeOfServiceId}`);
-  const body = await res.json();
+    const res = await fetch(
+        `${VITE_API_URL}/typeOfServices/${typeOfServiceId}`
+    );
+    const body = await res.json();
 
-  if (body.status === 'error') {
-    throw new Error(body.message);
-  }
+    if (body.status === 'error') {
+        throw new Error(body.message);
+    }
 
-  return body.data;
+    return body.data;
 };
 
-export const fetchAllTypeOfServices = async () => {
-  const res = await fetch(`${VITE_API_URL}/typeOfServices`);
-  const body = await res.json();
+export const fetchAllTypeOfServices = async (searchParamsToString) => {
+    const res = await fetch(
+        `${VITE_API_URL}/typeOfServices/?${searchParamsToString}`
+    );
+    const body = await res.json();
 
-  if (body.status === 'error') {
-    throw new Error(body.message);
-  }
+    if (body.status === 'error') {
+        throw new Error(body.message);
+    }
 
-  return body.data;
+    return body.data;
 };
