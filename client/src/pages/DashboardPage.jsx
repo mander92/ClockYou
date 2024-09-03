852d3bd851aa1230ba013b19244e379dba604a5c
fico.prieto <ficoprieto@ficoprieto.com>
2024-09-03 16:07:25 +0200

    retoques

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -12,102 +12,90 @@ import Requests from '../components/ClientDashboard/Requests';
 import Orders from '../components/ClientDashboard/Orders';
 
 const DashboardPage = () => {
-    const [activeSection, setActiveSection] = useState('profile');
-    const location = useLocation();
-    const { user } = useUser();
-    const userRole = user?.role;
+  const [activeSection, setActiveSection] = useState('profile');
+  const location = useLocation();
+  const { user } = useUser();
+  const userRole = user?.role;
 
-    useEffect(() => {
-        const hash = location.hash.replace('#', '');
-        if (hash) {
-            setActiveSection(hash);
-        }
-    }, [location]);
+  useEffect(() => {
+    const hash = location.hash.replace('#', '');
+    if (hash) {
+      setActiveSection(hash);
+    }
+  }, [location]);
 
-    const handleSectionChange = (section) => {
-        setActiveSection(section);
-    };
+  const handleSectionChange = (section) => {
+    setActiveSection(section);
+  };
 
-    const sectionComponents = {
-        profile: <Profile />,
-        users: userRole === 'admin' && <Users />,
-        services: userRole === 'admin' && <Services />,
-        contracts: userRole === 'admin' && <Contracts />,
-        shifts: userRole === 'admin' && <Shifts />,
-        requests: userRole === 'client' && <Requests />,
-        orders: userRole === 'client' && <Orders />,
-        myservices: userRole === 'employee' && <MyServices />,
-    };
+  const sectionComponents = {
+    profile: <Profile />,
+    users: userRole === 'admin' && <Users />,
+    services: userRole === 'admin' && <Services />,
+    contracts: userRole === 'admin' && <Contracts />,
+    shifts: userRole === 'admin' && <Shifts />,
+    requests: userRole === 'client' && <Requests />,
+    orders: userRole === 'client' && <Orders />,
+    myservices: userRole === 'employee' && <MyServices />,
+  };
 
-    return (
-        <section className='container'>
-            <Avatar />
+  return (
+    <section className='container formsWrapper'>
+      <Avatar />
 
-            <div>
-                <NavLink
-                    to='#profile'
-                    onClick={() => handleSectionChange('profile')}
-                >
-                    Mi Perfil
-                </NavLink>
+      <div>
+        <NavLink to='#profile' onClick={() => handleSectionChange('profile')}>
+          Mi Perfil
+        </NavLink>
 
-                {userRole === 'admin' && (
-                    <>
-                        <NavLink
-                            to='#users'
-                            onClick={() => handleSectionChange('users')}
-                        >
-                            Usuarios
-                        </NavLink>
-                        <NavLink
-                            to='#services'
-                            onClick={() => handleSectionChange('services')}
-                        >
-                            Servicios
-                        </NavLink>
-                        <NavLink
-                            to='#contracts'
-                            onClick={() => handleSectionChange('contracts')}
-                        >
-                            Contratos
-                        </NavLink>
-                        <NavLink
-                            to='#shifts'
-                            onClick={() => handleSectionChange('shifts')}
-                        >
-                            Turnos
-                        </NavLink>
-                    </>
-                )}
+        {userRole === 'admin' && (
+          <>
+            <NavLink to='#users' onClick={() => handleSectionChange('users')}>
+              Usuarios
+            </NavLink>
+            <NavLink
+              to='#services'
+              onClick={() => handleSectionChange('services')}
+            >
+              Servicios
+            </NavLink>
+            <NavLink
+              to='#contracts'
+              onClick={() => handleSectionChange('contracts')}
+            >
+              Contratos
+            </NavLink>
+            <NavLink to='#shifts' onClick={() => handleSectionChange('shifts')}>
+              Turnos
+            </NavLink>
+          </>
+        )}
 
-                {userRole === 'client' && (
-                    <>
-                        <NavLink
-                            to='#requests'
-                            onClick={() => handleSectionChange('requests')}
-                        >
-                            Solicitudes
-                        </NavLink>
-                        <NavLink
-                            to='#orders'
-                            onClick={() => handleSectionChange('orders')}
-                        >
-                            Pedidos
-                        </NavLink>
-                    </>
-                )}
-                {userRole === 'employee' && (
-                    <NavLink
-                        to='#myservices'
-                        onClick={() => handleSectionChange('myservies')}
-                    >
-                        Mis Servicios
-                    </NavLink>
-                )}
-            </div>
-            {sectionComponents[activeSection]}
-        </section>
-    );
+        {userRole === 'client' && (
+          <>
+            <NavLink
+              to='#requests'
+              onClick={() => handleSectionChange('requests')}
+            >
+              Solicitudes
+            </NavLink>
+            <NavLink to='#orders' onClick={() => handleSectionChange('orders')}>
+              Pedidos
+            </NavLink>
+          </>
+        )}
+        {userRole === 'employee' && (
+          <NavLink
+            to='#myservices'
+            onClick={() => handleSectionChange('myservies')}
+          >
+            Mis Servicios
+          </NavLink>
+        )}
+      </div>
+      {sectionComponents[activeSection]}
+    </section>
+  );
 };
 
 export default DashboardPage;
