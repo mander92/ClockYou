2175e367e482d6873227a56243ad7888a4db86b9
mander92 <mariomartinrodriguez92@gmail.com>
2024-09-03 10:29:00 +0200

    dashboardCAHNGES

---
+++ client/src/components/Avatar/Avatar.jsx
@@ -0,0 +1,81 @@
+const { VITE_API_URL } = import.meta.env;
+import useUser from '../../hooks/useUser.js';
+import { useState, useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext.jsx';
+import {fetchEditAvatarService} from '../../services/userServices.js';
+import toast from 'react-hot-toast';
+
+const Avatar = () => {
+
+    const { user } = useUser();
+    const { authToken } = useContext(AuthContext);
+    const userId = user?.id;
+
+    const [avatar, setAvatar] = useState(null);
+    const [enableEditAvatar, setEnableEditAvatar] = useState(false);
+
+    const handleEditAvatar = async (e) => {
+        e.preventDefault();
+        try {
+            if (enableEditAvatar) {
+                const data = await fetchEditAvatarService(
+                    userId,
+                    authToken,
+                    avatar
+                );
+                setAvatar(null);
+                toast.success(data.message, {
+                    id: 'ok',
+                });
+                window.location.reload();
+            }
+            setEnableEditAvatar(!enableEditAvatar);
+        } catch (error) {
+            toast.error(error.message, {
+                id: 'error',
+            });
+        }
+    }
+
+
+    return (
+        <>
+        <form className='form' onSubmit={handleEditAvatar}>
+            <fieldset>
+                <legend>Avatar</legend>
+                <img
+                    className='user-avatar'
+                    src={`${
+                        user?.avatar
+                            ? `${VITE_API_URL}/${user.avatar}`
+                            : '/default-avatar.png'
+                    }`}
+                    alt='Avatar'
+                />
+                {enableEditAvatar ? (
+                    <div>
+                        <input
+                            type='file'
+                            accept='image/png, image/jpg, image/jpeg, image/tiff'
+                            required
+                            onChange={(e) => {
+                                setAvatar(e.target.files[0]);
+                            }}
+                        />
+                    </div>
+                ) : (
+                    ''
+                )}
+                <div>
+                    <button type='submit'>
+                        {!enableEditAvatar ? 'Editar' : 'Guardar'}
+                    </button>
+                </div>
+            </fieldset>
+        </form>
+        </>
+    )
+
+}
+
+export default Avatar;
--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,267 +1,30 @@
-const { VITE_API_URL } = import.meta.env;
+
 import useUser from '../hooks/useUser';
-import { useState, useContext, useEffect } from 'react';
-import { AuthContext } from '../context/AuthContext';
-import {
-    fetchEditUserService,
-    fetchEditPasswordService,
-    fetchDeleteUserService,
-    fetchEditAvatarService,
-} from '../services/userServices';
-import toast from 'react-hot-toast';
-import { useNavigate, Navigate } from 'react-router-dom';
+import Avatar from '../components/Avatar/Avatar';
+import Profile from '../components/Profile/Profile';
 
 const DashboardPage = () => {
-    const { user } = useUser();
-    const { authToken, authLogout } = useContext(AuthContext);
-    const navigate = useNavigate();
-    const userId = user?.id;
 
-    const [avatar, setAvatar] = useState(null);
-    const [enableEditAvatar, setEnableEditAvatar] = useState(false);
-    const [firstName, setFirstName] = useState(user?.firstName || '');
-    const [lastName, setLastName] = useState(user?.lastName || '');
-    const [phone, setPhone] = useState(user?.phone || '');
-    const [actualPassword, setActualPassword] = useState('');
-    const [newPassword, setNewPassword] = useState('');
-    const [repeatedNewPassword, setRepeatedNewPassword] = useState('');
+    const {user} = useUser();
 
-    const handleEditAvatar = async (e) => {
-        e.preventDefault();
-        try {
-            if (enableEditAvatar) {
-                const data = await fetchEditAvatarService(
-                    userId,
-                    authToken,
-                    avatar
-                );
-                setAvatar(null);
-                toast.success(data.message, {
-                    id: 'ok',
-                });
-                window.location.reload();
-            }
-            setEnableEditAvatar(!enableEditAvatar);
-        } catch (error) {
-            toast.error(error.message, {
-                id: 'error',
-            });
-        }
-    };
 
-    const handleEditUser = async (e) => {
-        e.preventDefault();
-        try {
-            const data = await fetchEditUserService(
-                authToken,
-                firstName,
-                lastName,
-                phone,
-                userId
-            );
-            toast.success(data.message, {
-                id: 'ok',
-            });
-        } catch (error) {
-            toast.error(error.message, {
-                id: 'error',
-            });
-        }
-    };
 
-    const handleEditPassword = async (e) => {
-        e.preventDefault();
-        try {
-            if (newPassword !== repeatedNewPassword) {
-                throw new Error('¡Las nuevas contraseñas no coinciden!');
-            } else {
-                const data = await fetchEditPasswordService(
-                    authToken,
-                    actualPassword,
-                    newPassword,
-                    userId
-                );
-                toast.success(data.message, {
-                    id: 'ok',
-                });
-                setActualPassword('');
-                setNewPassword('');
-                setRepeatedNewPassword('');
-            }
-        } catch (error) {
-            toast.error(error.message, {
-                id: 'error',
-            });
-        }
-    };
-
-    const handleDeleteUser = async () => {
-        if (
-            window.confirm(
-                '¿Estás seguro de querer eliminar tu cuenta? ¡¡¡Esta acción no se puede deshacer!!!'
-            )
-        ) {
-            try {
-                const data = await fetchDeleteUserService(authToken, userId);
-                toast.success(data.message, {
-                    id: 'ok',
-                });
-                authLogout();
-                navigate('/');
-            } catch (error) {
-                toast.error(error.message, {
-                    id: 'error',
-                });
-            }
-        }
-    };
-
-    useEffect(() => {
-        if (user) {
-            setFirstName(user?.firstName);
-            setLastName(user?.lastName);
-            setPhone(user?.phone);
-        }
-    }, [user]);
-
-    if (!authToken) return <Navigate to='/' />;
 
     return (
         <section className='container'>
-            <form className='form' onSubmit={handleEditAvatar}>
-                <fieldset>
-                    <legend>Avatar</legend>
-                    <img
-                        className='user-avatar'
-                        src={`${
-                            user?.avatar
-                                ? `${VITE_API_URL}/${user.avatar}`
-                                : '/default-avatar.png'
-                        }`}
-                        alt='Avatar'
-                    />
-                    {enableEditAvatar ? (
-                        <div>
-                            <input
-                                type='file'
-                                accept='image/png, image/jpg, image/jpeg, image/tiff'
-                                required
-                                onChange={(e) => {
-                                    setAvatar(e.target.files[0]);
-                                }}
-                            />
-                        </div>
-                    ) : (
-                        ''
-                    )}
-                    <div>
-                        <button type='submit'>
-                            {!enableEditAvatar ? 'Editar' : 'Guardar'}
-                        </button>
-                    </div>
-                </fieldset>
-            </form>
-            <form className='form' onSubmit={handleEditUser}>
-                <fieldset>
-                    <legend>Perfil</legend>
-                    <label htmlFor='email'>Email</label>
-                    <input disabled value={user?.email || ''} />
-                    <label htmlFor='firstName'>Nombre</label>
-                    <input
-                        type='text'
-                        id='firstName'
-                        value={firstName || ''}
-                        onChange={(e) => {
-                            setFirstName(e.target.value);
-                        }}
-                        required
-                    />
-                    <label htmlFor='lastName'>Apellidos</label>
-                    <input
-                        type='text'
-                        id='lastName'
-                        value={lastName}
-                        onChange={(e) => {
-                            setLastName(e.target.value);
-                        }}
-                        required
-                    />
-                    <label htmlFor='dni'>DNI</label>
-                    <input disabled value={user?.dni || ''} />
-                    <label htmlFor='phone'>Teléfono</label>
-                    <input
-                        type='tel'
-                        id='phone'
-                        value={phone}
-                        onChange={(e) => {
-                            setPhone(e.target.value);
-                        }}
-                        required
-                    />
-                    {user?.role === 'employee' && (
-                        <>
-                            <label htmlFor='job'>Trabajo</label>
-                            <input disabled value={user?.job || ''} />
-                            <label htmlFor='city'>Ciudad</label>
-                            <input disabled value={user?.city || ''} />
-                        </>
-                    )}
-                    <div>
-                        <button type='submit'>Guardar Cambios</button>
-                    </div>
-                </fieldset>
-            </form>
-            <form className='form' onSubmit={handleEditPassword}>
-                <fieldset>
-                    <legend>Contraseña</legend>
-                    <label htmlFor='actualPassword'>Contraseña Actual</label>
-                    <input
-                        type='password'
-                        id='actualPassword'
-                        value={actualPassword}
-                        placeholder='jobryp-kapDew-fetho6'
-                        required
-                        onChange={(e) => {
-                            setActualPassword(e.target.value);
-                        }}
-                    />
-                    <label htmlFor='newPassword'>Nueva Contraseña</label>
-                    <input
-                        type='password'
-                        id='newPassword'
-                        value={newPassword}
-                        placeholder='bemgon-1bizni-nuhXyd'
-                        required
-                        onChange={(e) => {
-                            setNewPassword(e.target.value);
-                        }}
-                    />
-                    <label htmlFor='repeatNewPassword'>
-                        Repetir Contraseña
-                    </label>
-                    <input
-                        type='password'
-                        id='repeatNewPassword'
-                        placeholder='bemgon-1bizni-nuhXyd'
-                        required
-                        value={repeatedNewPassword}
-                        onChange={(e) => {
-                            setRepeatedNewPassword(e.target.value);
-                        }}
-                    />
-                    <div>
-                        <button type='submit'>Cambiar Contraseña</button>
-                    </div>
-                </fieldset>
-            </form>
-            <form className='form' onSubmit={handleDeleteUser}>
-                <fieldset>
-                    <legend>Cuenta</legend>
-                    <div>
-                        <button type='submit'>Eliminar Usuario</button>
-                    </div>
-                </fieldset>
-            </form>
+            <Avatar />
+
+            <button>Mi Perfil</button>
+            <button>Usuarios</button>
+            <button>Servicios</button>
+            <button>Contratos</button>
+            <button>Turnos</button>
+
+
+            <Profile />
+            
+        
+            
         </section>
     );
 };
