c5b8f867a070e88e7e76205388b94a07ba219019
fico.prieto <ficoprieto@ficoprieto.com>
2024-09-05 08:20:19 +0200

    maquetación sección -mi perfil- + ajustes css

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -2,206 +2,206 @@ import { useState, useContext, useEffect } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import {
-    fetchEditUserService,
-    fetchEditPasswordService,
-    fetchDeleteUserService,
+  fetchEditUserService,
+  fetchEditPasswordService,
+  fetchDeleteUserService,
 } from '../../services/userServices';
 import useUser from '../../hooks/useUser';
 import toast from 'react-hot-toast';
 
 const Profile = () => {
-    const { user } = useUser();
-    const { authToken, authLogout } = useContext(AuthContext);
-    const userId = user?.id;
-    const navigate = useNavigate();
+  const { user } = useUser();
+  const { authToken, authLogout } = useContext(AuthContext);
+  const userId = user?.id;
+  const navigate = useNavigate();
 
-    const [firstName, setFirstName] = useState(user?.firstName || '');
-    const [lastName, setLastName] = useState(user?.lastName || '');
-    const [phone, setPhone] = useState(user?.phone || '');
-    const [actualPassword, setActualPassword] = useState('');
-    const [newPassword, setNewPassword] = useState('');
-    const [repeatedNewPassword, setRepeatedNewPassword] = useState('');
+  const [firstName, setFirstName] = useState(user?.firstName || '');
+  const [lastName, setLastName] = useState(user?.lastName || '');
+  const [phone, setPhone] = useState(user?.phone || '');
+  const [actualPassword, setActualPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [repeatedNewPassword, setRepeatedNewPassword] = useState('');
 
-    const handleEditUser = async (e) => {
-        e.preventDefault();
-        try {
-            const data = await fetchEditUserService(
-                authToken,
-                firstName,
-                lastName,
-                phone,
-                userId
-            );
-            toast.success(data.message, {
-                id: 'ok',
-            });
-        } catch (error) {
-            toast.error(error.message, {
-                id: 'error',
-            });
-        }
-    };
+  const handleEditUser = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await fetchEditUserService(
+        authToken,
+        firstName,
+        lastName,
+        phone,
+        userId
+      );
+      toast.success(data.message, {
+        id: 'ok',
+      });
+    } catch (error) {
+      toast.error(error.message, {
+        id: 'error',
+      });
+    }
+  };
 
-    const handleEditPassword = async (e) => {
-        e.preventDefault();
-        try {
-            if (newPassword !== repeatedNewPassword) {
-                throw new Error('¡Las nuevas contraseñas no coinciden!');
-            } else {
-                const data = await fetchEditPasswordService(
-                    authToken,
-                    actualPassword,
-                    newPassword,
-                    userId
-                );
-                toast.success(data.message, {
-                    id: 'ok',
-                });
-                setActualPassword('');
-                setNewPassword('');
-                setRepeatedNewPassword('');
-            }
-        } catch (error) {
-            toast.error(error.message, {
-                id: 'error',
-            });
-        }
-    };
+  const handleEditPassword = async (e) => {
+    e.preventDefault();
+    try {
+      if (newPassword !== repeatedNewPassword) {
+        throw new Error('¡Las nuevas contraseñas no coinciden!');
+      } else {
+        const data = await fetchEditPasswordService(
+          authToken,
+          actualPassword,
+          newPassword,
+          userId
+        );
+        toast.success(data.message, {
+          id: 'ok',
+        });
+        setActualPassword('');
+        setNewPassword('');
+        setRepeatedNewPassword('');
+      }
+    } catch (error) {
+      toast.error(error.message, {
+        id: 'error',
+      });
+    }
+  };
 
-    const handleDeleteUser = async () => {
-        if (
-            window.confirm(
-                '¿Estás seguro de querer eliminar tu cuenta? ¡¡¡Esta acción no se puede deshacer!!!'
-            )
-        ) {
-            try {
-                const data = await fetchDeleteUserService(authToken, userId);
-                toast.success(data.message, {
-                    id: 'ok',
-                });
-                authLogout();
-                navigate('/');
-            } catch (error) {
-                toast.error(error.message, {
-                    id: 'error',
-                });
-            }
-        }
-    };
+  const handleDeleteUser = async () => {
+    if (
+      window.confirm(
+        '¿Estás seguro de querer eliminar tu cuenta? ¡¡¡Esta acción no se puede deshacer!!!'
+      )
+    ) {
+      try {
+        const data = await fetchDeleteUserService(authToken, userId);
+        toast.success(data.message, {
+          id: 'ok',
+        });
+        authLogout();
+        navigate('/');
+      } catch (error) {
+        toast.error(error.message, {
+          id: 'error',
+        });
+      }
+    }
+  };
 
-    useEffect(() => {
-        if (user) {
-            setFirstName(user?.firstName);
-            setLastName(user?.lastName);
-            setPhone(user?.phone);
-        }
-    }, [user]);
+  useEffect(() => {
+    if (user) {
+      setFirstName(user?.firstName);
+      setLastName(user?.lastName);
+      setPhone(user?.phone);
+    }
+  }, [user]);
 
-    return (
-        <div>
-            <form className='form' onSubmit={handleEditUser}>
-                <fieldset>
-                    <legend>Perfil</legend>
-                    <label htmlFor='email'>Email</label>
-                    <input disabled value={user?.email || ''} />
-                    <label htmlFor='firstName'>Nombre</label>
-                    <input
-                        type='text'
-                        id='firstName'
-                        value={firstName || ''}
-                        onChange={(e) => {
-                            setFirstName(e.target.value);
-                        }}
-                        required
-                    />
-                    <label htmlFor='lastName'>Apellidos</label>
-                    <input
-                        type='text'
-                        id='lastName'
-                        value={lastName}
-                        onChange={(e) => {
-                            setLastName(e.target.value);
-                        }}
-                        required
-                    />
-                    <label htmlFor='dni'>DNI</label>
-                    <input disabled value={user?.dni || ''} />
-                    <label htmlFor='phone'>Teléfono</label>
-                    <input
-                        type='tel'
-                        id='phone'
-                        value={phone}
-                        onChange={(e) => {
-                            setPhone(e.target.value);
-                        }}
-                        required
-                    />
-                    {user?.role === 'employee' && (
-                        <>
-                            <label htmlFor='job'>Trabajo</label>
-                            <input disabled value={user?.job || ''} />
-                            <label htmlFor='city'>Ciudad</label>
-                            <input disabled value={user?.city || ''} />
-                        </>
-                    )}
-                    <div>
-                        <button type='submit'>Guardar Cambios</button>
-                    </div>
-                </fieldset>
-            </form>
-            <form className='form' onSubmit={handleEditPassword}>
-                <fieldset>
-                    <legend>Contraseña</legend>
-                    <label htmlFor='actualPassword'>Contraseña Actual</label>
-                    <input
-                        type='password'
-                        id='actualPassword'
-                        value={actualPassword}
-                        placeholder='jobryp-kapDew-fetho6'
-                        required
-                        onChange={(e) => {
-                            setActualPassword(e.target.value);
-                        }}
-                    />
-                    <label htmlFor='newPassword'>Nueva Contraseña</label>
-                    <input
-                        type='password'
-                        id='newPassword'
-                        value={newPassword}
-                        placeholder='bemgon-1bizni-nuhXyd'
-                        required
-                        onChange={(e) => {
-                            setNewPassword(e.target.value);
-                        }}
-                    />
-                    <label htmlFor='repeatNewPassword'>
-                        Repetir Contraseña
-                    </label>
-                    <input
-                        type='password'
-                        id='repeatNewPassword'
-                        placeholder='bemgon-1bizni-nuhXyd'
-                        required
-                        value={repeatedNewPassword}
-                        onChange={(e) => {
-                            setRepeatedNewPassword(e.target.value);
-                        }}
-                    />
-                    <div>
-                        <button type='submit'>Cambiar Contraseña</button>
-                    </div>
-                </fieldset>
-            </form>
-            <form className='form' onSubmit={handleDeleteUser}>
-                <fieldset>
-                    <legend>Cuenta</legend>
-                    <div>
-                        <button type='submit'>Eliminar Usuario</button>
-                    </div>
-                </fieldset>
-            </form>
-        </div>
-    );
+  return (
+    <div className='editServiceLayoutWrapper'>
+      <form className='form' id='perfilUsuario' onSubmit={handleEditUser}>
+        <fieldset>
+          <legend>Perfil</legend>
+          <label htmlFor='email'>Email</label>
+          <input disabled value={user?.email || ''} />
+          <label htmlFor='firstName'>Nombre</label>
+          <input
+            type='text'
+            id='firstName'
+            value={firstName || ''}
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+            required
+          />
+          <label htmlFor='lastName'>Apellidos</label>
+          <input
+            type='text'
+            id='lastName'
+            value={lastName}
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+            required
+          />
+          <label htmlFor='dni'>DNI</label>
+          <input disabled value={user?.dni || ''} />
+          <label htmlFor='phone'>Teléfono</label>
+          <input
+            type='tel'
+            id='phone'
+            value={phone}
+            onChange={(e) => {
+              setPhone(e.target.value);
+            }}
+            required
+          />
+          {user?.role === 'employee' && (
+            <>
+              <label htmlFor='job'>Trabajo</label>
+              <input disabled value={user?.job || ''} />
+              <label htmlFor='city'>Ciudad</label>
+              <input disabled value={user?.city || ''} />
+            </>
+          )}
+          <div>
+            <button type='submit'>Guardar Cambios</button>
+          </div>
+        </fieldset>
+      </form>
+      <section>
+        <form className='form' onSubmit={handleEditPassword}>
+          <fieldset>
+            <legend>Contraseña</legend>
+            <label htmlFor='actualPassword'>Contraseña Actual</label>
+            <input
+              type='password'
+              id='actualPassword'
+              value={actualPassword}
+              placeholder='jobryp-kapDew-fetho6'
+              required
+              onChange={(e) => {
+                setActualPassword(e.target.value);
+              }}
+            />
+            <label htmlFor='newPassword'>Nueva Contraseña</label>
+            <input
+              type='password'
+              id='newPassword'
+              value={newPassword}
+              placeholder='bemgon-1bizni-nuhXyd'
+              required
+              onChange={(e) => {
+                setNewPassword(e.target.value);
+              }}
+            />
+            <label htmlFor='repeatNewPassword'>Repetir Contraseña</label>
+            <input
+              type='password'
+              id='repeatNewPassword'
+              placeholder='bemgon-1bizni-nuhXyd'
+              required
+              value={repeatedNewPassword}
+              onChange={(e) => {
+                setRepeatedNewPassword(e.target.value);
+              }}
+            />
+            <div>
+              <button type='submit'>Cambiar Contraseña</button>
+            </div>
+          </fieldset>
+        </form>
+        <form className='form' onSubmit={handleDeleteUser}>
+          <fieldset>
+            <legend>Cuenta</legend>
+            <div>
+              <button type='submit'>Eliminar Usuario</button>
+            </div>
+          </fieldset>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default Profile;
--- client/src/pages/EditService.jsx
+++ client/src/pages/EditService.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect, useContext } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import {
-    fetchTypeOfService,
-    fetchDeleteTypeOfServices,
-    fetchEditTypeOfServices,
+  fetchTypeOfService,
+  fetchDeleteTypeOfServices,
+  fetchEditTypeOfServices,
 } from '../services/typeOfServiceServices';
 import { AuthContext } from '../context/AuthContext';
 import toast from 'react-hot-toast';
@@ -11,116 +11,111 @@ const { VITE_API_URL } = import.meta.env;
 import './editPagesForms.css';
 
 const EditService = () => {
-    const { typeOfServiceId } = useParams();
-    const [data, setData] = useState(null);
-    const [description, setDescription] = useState(data?.description || '');
-    const [price, setPrice] = useState(data?.price || '');
+  const { typeOfServiceId } = useParams();
+  const [data, setData] = useState(null);
+  const [description, setDescription] = useState(data?.description || '');
+  const [price, setPrice] = useState(data?.price || '');
 
-    const { authToken } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const { authToken } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const getTypeOfService = async () => {
-            try {
-                const data = await fetchTypeOfService(typeOfServiceId);
-                setData(data);
-                setDescription(data.description);
-                setPrice(data.price);
-            } catch (error) {
-                toast.error(error.message, {
-                    id: 'error',
-                });
-            }
-        };
-
-        getTypeOfService();
-    }, [typeOfServiceId]);
-
-    const handleEditService = async (e) => {
-        e.preventDefault();
-
-        try {
-            const body = await fetchEditTypeOfServices(
-                typeOfServiceId,
-                description,
-                price,
-                authToken
-            );
-            toast.success(body.message, {
-                id: 'ok',
-            });
-            navigate('/user#services');
-        } catch (error) {
-            toast.error(error.message, {
-                id: 'error',
-            });
-        }
+  useEffect(() => {
+    const getTypeOfService = async () => {
+      try {
+        const data = await fetchTypeOfService(typeOfServiceId);
+        setData(data);
+        setDescription(data.description);
+        setPrice(data.price);
+      } catch (error) {
+        toast.error(error.message, {
+          id: 'error',
+        });
+      }
     };
 
-    const handleDeleteService = async () => {
-        try {
-            const body = await fetchDeleteTypeOfServices(
-                typeOfServiceId,
-                authToken
-            );
-            toast.success(body.message, {
-                id: 'ok',
-            });
-            navigate('/user#services');
-        } catch (error) {
-            toast.error(error.message, {
-                id: 'error',
-            });
-        }
-    };
+    getTypeOfService();
+  }, [typeOfServiceId]);
 
-    return (
-        <section className='container editServiceLayoutWrapper'>
-            <article className='editServiceLayout'>
-                <img
-                    src={`${VITE_API_URL}/${data?.image}`}
-                    alt={`${data?.description}`}
-                />
-                <h3 className='container'>
-                    {data?.type} En {data?.city}
-                </h3>
-            </article>
+  const handleEditService = async (e) => {
+    e.preventDefault();
 
-            <form className='form' onSubmit={handleEditService}>
-                <fieldset>
-                    <legend>Modificar Servicio</legend>
+    try {
+      const body = await fetchEditTypeOfServices(
+        typeOfServiceId,
+        description,
+        price,
+        authToken
+      );
+      toast.success(body.message, {
+        id: 'ok',
+      });
+      navigate('/user#services');
+    } catch (error) {
+      toast.error(error.message, {
+        id: 'error',
+      });
+    }
+  };
 
-                    <label htmlFor='description'>Descripción</label>
-                    <input
-                        id='description'
-                        type='text'
-                        value={description}
-                        onChange={(e) => {
-                            setDescription(e.target.value);
-                        }}
-                        placeholder={data?.description}
-                        required
-                    />
+  const handleDeleteService = async () => {
+    try {
+      const body = await fetchDeleteTypeOfServices(typeOfServiceId, authToken);
+      toast.success(body.message, {
+        id: 'ok',
+      });
+      navigate('/user#services');
+    } catch (error) {
+      toast.error(error.message, {
+        id: 'error',
+      });
+    }
+  };
 
-                    <label htmlFor='description'>Precio</label>
-                    <input
-                        type='number'
-                        value={price}
-                        onChange={(e) => {
-                            setPrice(e.target.value);
-                        }}
-                        placeholder={data?.price}
-                        required
-                    />
+  return (
+    <section className='container editServiceLayoutWrapper'>
+      <article className='editServiceLayout'>
+        <img
+          src={`${VITE_API_URL}/${data?.image}`}
+          alt={`${data?.description}`}
+        />
+        <h3 className=''>
+          {data?.type} En {data?.city}
+        </h3>
+      </article>
 
-                    <button>Guardar Cambios</button>
-                    <button onClick={handleDeleteService}>
-                        Eliminar Servicio
-                    </button>
-                </fieldset>
-            </form>
-        </section>
-    );
+      <form className='form' onSubmit={handleEditService}>
+        <fieldset>
+          <legend>Modificar Servicio</legend>
+
+          <label htmlFor='description'>Descripción</label>
+          <input
+            id='description'
+            type='text'
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+            placeholder={data?.description}
+            required
+          />
+
+          <label htmlFor='description'>Precio</label>
+          <input
+            type='number'
+            value={price}
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+            placeholder={data?.price}
+            required
+          />
+
+          <button>Guardar Cambios</button>
+          <button onClick={handleDeleteService}>Eliminar Servicio</button>
+        </fieldset>
+      </form>
+    </section>
+  );
 };
 
 export default EditService;
