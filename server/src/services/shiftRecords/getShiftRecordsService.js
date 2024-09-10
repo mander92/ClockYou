aa593ebc29ce171eefb77d4c7160e4c2c460ea65
Marc-Olle <marc.olle01@gmail.com>
2024-09-10 17:59:37 +0200

    getshift

--- server/src/services/shiftRecords/getShiftRecordsService.js
+++ server/src/services/shiftRecords/getShiftRecordsService.js
@@ -1,50 +1,30 @@
 import getPool from '../../db/getPool.js';
 
-const getShiftRecordsService = async (serviceId, employeeId) => {
+const getShiftRecordsService = async (shiftRecordId) => {
     const pool = await getPool();
 
-    if (!serviceId && !employeeId) {
-        const [shifts] = await pool.query(
-            `
-            SELECT * from shiftRecords;
-            `
-        );
+    let sqlQuery = `
+        SELECT 
+            u.firstName, 
+            u.lastName, 
+            se.totalPrice, 
+            a.city, 
+            a.address, 
+            t.type 
+        FROM 
+            shiftRecords s
+        INNER JOIN 
+            users u ON u.id = s.employeeId
+        INNER JOIN 
+            services se ON se.id = s.serviceId
+        INNER JOIN 
+            addresses a ON a.id = se.addressId
+        INNER JOIN 
+            typeOfServices t ON t.id = se.typeOfServicesId
+    `;
 
-        return shifts;
-    }
-
-    if (serviceId && employeeId) {
-        const [shifts] = await pool.query(
-            `
-            SELECT * FROM shiftRecords WHERE serviceId = ? AND employeeId = ?
-            `,
-            [serviceId, employeeId]
-        );
-
-        return shifts;
-    }
-
-    if (employeeId) {
-        const [shifts] = await pool.query(
-            `
-            SELECT * FROM shiftRecords WHERE employeeId = ? 
-            `,
-            [employeeId]
-        );
-
-        return shifts;
-    }
-
-    if (serviceId) {
-        const [shifts] = await pool.query(
-            `
-            SELECT * FROM shiftRecords WHERE serviceId = ? 
-            `,
-            [serviceId]
-        );
-
-        return shifts;
-    }
+    const [shift] = await pool.query(sqlQuery, sqlValues);
+    return shift;
 };
 
 export default getShiftRecordsService;
