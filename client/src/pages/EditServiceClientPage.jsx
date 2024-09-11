4ff3754eced5c6fbdfb36aba2e244cae41a1e316
Thanh Hang Nguyen <zizie.nguyen@gmail.com>
2024-09-11 11:12:31 +0200

    button editar Cli

--- client/src/pages/EditServiceClientPage.jsx
+++ client/src/pages/EditServiceClientPage.jsx
@@ -38,8 +38,7 @@ const EditServiceClientPage = () => {
         const getService = async () => {
             try {
                 const data = await fetchClientServiceServices(
-                    serviceId,
-                    authToken
+                    serviceId
                 );
                 setData(data);
                 setHours(data.hours);
@@ -56,32 +55,65 @@ const EditServiceClientPage = () => {
         };
 
         getService();
-    }, []);
+    }, [serviceId]);
 
-    // const handleEditService = async (e) => {
-    //     e.preventDefault();
-    //     try {
-    //         const body = await fetchEditServiceService(
-    //             serviceId,
-    //             comments,
-    //             address,
-    //             hours
-    //         );
-    //         toast.success(body.message, {
-    //             id: 'ok',
-    //         });
-    //         navigate('/orders');
-    //     } catch (error) {
-    //         toast.error(error.message, {
-    //             id: 'error',
-    //         });
-    //     }
-    // };
+    const handleEditService = async (e) => {
+        e.preventDefault();
+        try {
+            const body = await fetchEditServiceService(
+                serviceId,
+                comments,
+                address,
+                hours,
+                authToken
+            );
+            toast.success(body.message, {
+                id: 'ok',
+            });
+            navigate('/orders');
+        } catch (error) {
+            toast.error(error.message, {
+                id: 'error',
+            });
+        }
+    };
 
     return (
-        <div>
-            <h1>Hola</h1>
-        </div>
+        <form className='profile-form mx-auto'>
+            <fieldset>
+            <legend>Editar</legend>
+                <div>
+                    <label htmlFor='comments'>Comentarios:</label>
+                    <textarea
+                        value={comments}
+                        onChange={(e) => setComments(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='address'>Dirección:</label>
+                    <input
+                        type="text"
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='hours'>Horas Contratadas:</label>
+                    <input
+                        type="number"
+                        value={hours}
+                        onChange={(e) => setHours(e.target.value)}
+                    />
+                </div>
+                <div className='mx-auto'>
+                    <button
+                        className='mr-4 mt-4'
+                        type='submit'
+                        onClick={handleEditService}>Guardar Cambios
+                    </button>
+                </div>
+            </fieldset>
+        </form>
     );
 };
 
