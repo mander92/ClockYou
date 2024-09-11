b2bb2844ac2f85b696ffdfd8db7421a965af69cf
Thanh Hang Nguyen <zizie.nguyen@gmail.com>
2024-09-11 11:55:33 +0200

    ruta front RatingPage Cli

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import DetailServicePage from './pages/DetailServicePage.jsx';
 import ConfirmedService from './pages/ConfirmedService.jsx';
 import Clock from './pages/Clock.jsx';
 import EditServiceClientPage from './pages/EditServiceClientPage.jsx';
+import RatingServiceClientPage from './pages/RatingServiceClientPage.jsx';
 
 const App = () => {
     return (
@@ -52,6 +53,11 @@ const App = () => {
                     <Route
                         path='/services/edit/:serviceId' //Thanh
                         element={<EditServiceClientPage />}
+                    />
+
+                    <Route
+                        path='/services/rating/:serviceId' //Thanh
+                        element={<RatingServiceClientPage />}
                     />
 
                     <Route
