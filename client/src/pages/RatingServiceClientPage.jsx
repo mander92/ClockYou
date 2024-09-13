d05556948bc3388d483ad21ea2a496434ddde61b
fico.prieto <ficoprieto@ficoprieto.com>
2024-09-13 11:07:22 +0200

    retoques css responsive admin dashboard

--- client/src/pages/RatingServiceClientPage.jsx
+++ client/src/pages/RatingServiceClientPage.jsx
@@ -5,16 +5,15 @@ import {
     fetchClientRatingServiceServices,
     fetchRatingServiceServices,
 } from '../services/serviceServices';
-import {FaStar} from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
 import toast from 'react-hot-toast';
-
 
 const RatingServiceClientPage = () => {
     const { serviceId } = useParams();
     const { authToken } = useContext(AuthContext);
     const navigate = useNavigate();
 
-    const [data, setData] = useState(null);  
+    const [data, setData] = useState(null);
     const [rating, setRating] = useState(null);
     const [hover, setHover] = useState(null);
 
@@ -27,7 +26,7 @@ const RatingServiceClientPage = () => {
             } catch (error) {
                 toast.error(error.message, {
                     id: 'error',
-                });                
+                });
             }
         };
         getRating();
@@ -37,7 +36,6 @@ const RatingServiceClientPage = () => {
         e.preventDefault();
 
         try {
-
             const body = await fetchRatingServiceServices(
                 serviceId,
                 rating,
@@ -57,12 +55,23 @@ const RatingServiceClientPage = () => {
     return (
         <form className='mx-auto'>
             <fieldset>
-                <legend className=" text-xl font-semibold mb-4">Valorar nuestro servicio</legend>
+                <legend className=' text-xl font-semibold mb-4'>
+                    Valorar nuestro servicio
+                </legend>
                 <div className='my-4'>
-                    <h3>{data?.type} en {data?.province}</h3>
-                    <p><strong>Precio por Hora:</strong> {data?.price}€</p>
-                    <p><strong>Horas Contratadas:</strong> {data?.hours}</p>
-                    <p><strong>Dirección:</strong> {data?.address}, {data?.postCode}, {data?.city}</p>
+                    <h3>
+                        {data?.type} en {data?.province}
+                    </h3>
+                    <p>
+                        <strong>Precio por Hora:</strong> {data?.price}€
+                    </p>
+                    <p>
+                        <strong>Horas Contratadas:</strong> {data?.hours}
+                    </p>
+                    <p>
+                        <strong>Dirección:</strong> {data?.address},{' '}
+                        {data?.postCode}, {data?.city}
+                    </p>
                 </div>
 
                 <div className='flex justify-center mb-4'>
@@ -73,8 +82,8 @@ const RatingServiceClientPage = () => {
                                 <label key={currentRating}></label>
                                 <input
                                     className='hidden'
-                                    type="radio"
-                                    name="rating"
+                                    type='radio'
+                                    name='rating'
                                     value={currentRating}
                                     checked={rating === currentRating}
                                     onChange={() => setRating(currentRating)}
@@ -82,28 +91,32 @@ const RatingServiceClientPage = () => {
                                 <FaStar
                                     className='cursor-pointer'
                                     size={30}
-                                    color={currentRating <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+                                    color={
+                                        currentRating <= (hover || rating)
+                                            ? '#ffc107'
+                                            : '#e4e5e9'
+                                    }
                                     onMouseEnter={() => setHover(currentRating)}
                                     onMouseLeave={() => setHover(null)}
-                                    onClick={() => setRating(currentRating)} 
-                                />                       
-                            </>    
+                                    onClick={() => setRating(currentRating)}
+                                />
+                            </>
                         );
-                    })}               
+                    })}
                 </div>
-                
 
-                
                 <div className='mx-auto'>
                     <button
                         className='mr-4 mt-4'
-                        type="submit"
-                        onClick={handleRatingService}>Enviar
+                        type='submit'
+                        onClick={handleRatingService}
+                    >
+                        Enviar
                     </button>
                 </div>
             </fieldset>
-    </form> 
-    )
-}
+        </form>
+    );
+};
 
 export default RatingServiceClientPage;
