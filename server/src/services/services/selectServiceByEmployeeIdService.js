2c40e72fce344021c7b7a140a33ec7f16a57746a
David Mirás <david_miras@icloud.com>
2024-09-20 19:23:06 +0200

    Corrección

--- server/src/services/services/selectServiceByEmployeeIdService.js
+++ server/src/services/services/selectServiceByEmployeeIdService.js
@@ -17,7 +17,7 @@ const selectServiceByEmployeeIdService = async (employeeId) => {
         ON u.id = s.clientId
         INNER JOIN typeOfServices t
         ON t.id = s.typeOfServicesId
-        WHERE sr.employeeId = ? AND s.status = 'confirmed' OR 'completed'
+        WHERE sr.employeeId = ? AND (s.status = 'confirmed' OR s.status = 'completed')
         ORDER BY s.modifiedAt DESC
         `,
         [employeeId]
